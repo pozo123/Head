3b8f82239059913986327f909c77841fd6bfaf82
pozo123 <diego.pozo@hotmail.com>
2018-09-20 23:36:20 -0500

    Update app_inge.js

--- app_inge.js
+++ app_inge.js
@@ -10,6 +10,29 @@ var id_registrar = "registrar";
 var id_imprimir = "imprimir";
 var rama_bd = "inges";
 
+var rama_bd_proys = "proys";
+var id_proys = "proyectoAsignado";
+var id_elec = "checkboxElectricidad";
+var id_plom = "checkboxPlomeria";
+var id_admin = "inlineRadio1";
+var id_lider = "inlineRadio2";
+var id_proyectista = "inlineRadio3";
+
+$(document).ready(function(){
+    var select = document.getElementById(id_proys);
+    var option = document.createElement('option');
+    option.style = "display:none";
+    option.text = option.value = "";
+    select.appendChild(option);
+    
+    firebase.database().ref('' + rama_bd_proys).orderByChild('nombre').on('child_added',function(snapshot){
+        
+        var proy = snapshot.val();
+        var option2 = document.createElement('option');
+        option2.text = option2.value = proy.nombre; 
+        select.appendChild(option2);
+    });
+});
 
 $('#' + id_registrar).click((function () {
 
@@ -41,9 +64,24 @@ $('#' + id_imprimir).click((function () {
             //console.log(user.nombre);
             $('#' + id_email).val(user.email);
             $('#' + id_password).val(user.password);
-            $('#' + id_especialidad).val(user.especialidad);
-            $('#' + id_proyecto).val(user.proyecto);
-            $('#' + id_credenciales).val(user.credenciales);
+            if(user.especialidad == 1)
+                document.getElementById(id_elec).checked = true;
+            else if(user.especialidad == 2)
+                document.getElementById(id_plom).checked = true;
+            else if(user.especialidad == 3){
+                document.getElementById(id_elec).checked = true;
+                document.getElementById(id_plom).checked = true;
+            }
+                
+            //$('#' + id_proyecto).val(user.proyecto);
+            
+            if(user.credenciales == 1)
+                document.getElementById(id_admin).checked = true;
+            else if(user.credenciales == 2)
+                document.getElementById(id_lider).checked = true;
+            else if(user.credenciales == 3)
+                document.getElementById(id_proyectista).checked = true;
+                
             $('#' + id_uid).val(user.uid);
         })
     }
@@ -53,9 +91,24 @@ $('#' + id_imprimir).click((function () {
             //console.log(user.nombre);
             $('#' + id_nombre).val(user.nombre);
             $('#' + id_password).val(user.password);
-            $('#' + id_especialidad).val(user.especialidad);
-            $('#' + id_proyecto).val(user.proyecto);
-            $('#' + id_credenciales).val(user.credenciales);
+            if(user.especialidad == 1)
+                document.getElementById(id_elec).checked = true;
+            else if(user.especialidad == 2)
+                document.getElementById(id_plom).checked = true;
+            else if(user.especialidad == 3){
+                document.getElementById(id_elec).checked = true;
+                document.getElementById(id_plom).checked = true;
+            }
+                
+            //$('#' + id_proyecto).val(user.proyecto);
+            
+            if(user.credenciales == 1)
+                document.getElementById(id_admin).checked = true;
+            else if(user.credenciales == 2)
+                document.getElementById(id_lider).checked = true;
+            else if(user.credenciales == 3)
+                document.getElementById(id_proyectista).checked = true;
+            
             $('#' + id_uid).val(user.uid);
         })
     }
@@ -66,22 +119,55 @@ $('#' + id_imprimir).click((function () {
             $('#' + id_nombre).val(user.nombre);
             $('#' + id_email).val(user.email);
             $('#' + id_password).val(user.password);
-            $('#' + id_especialidad).val(user.especialidad);
-            $('#' + id_proyecto).val(user.proyecto);
-            $('#' + id_credenciales).val(user.credenciales);
+            if(user.especialidad == 1)
+                document.getElementById(id_elec).checked = true;
+            else if(user.especialidad == 2)
+                document.getElementById(id_plom).checked = true;
+            else if(user.especialidad == 3){
+                document.getElementById(id_elec).checked = true;
+                document.getElementById(id_plom).checked = true;
+            }
+                
+            //$('#' + id_proyecto).val(user.proyecto);
+            
+            if(user.credenciales == 1)
+                document.getElementById(id_admin).checked = true;
+            else if(user.credenciales == 2)
+                document.getElementById(id_lider).checked = true;
+            else if(user.credenciales == 3)
+                document.getElementById(id_proyectista).checked = true;
         })
     }
 }))
 
 function guardaDatos(user) {
+    var espec;
+    var creden;
+    if(document.getElementById(id_elec).checked == true){
+        if(document.getElementById(id_plom).checked == true){
+            espec = 3; //especialidad ambas
+        }
+        else
+            espec = 1; //especialidad elec
+    }
+    else if(document.getElementById(id_plom).checked == true)
+        espec = 2; //especialidad plom
+    
+    if(document.getElementById(id_admin).checked == true)
+        creden = 1;
+    else if(document.getElementById(id_lider).checked == true)
+        creden = 2;
+    else if(document.getElementById(id_proyectista).checked == true)
+        creden = 3;
+    
     var usuario = {
         uid: user.uid,
         nombre: $('#' + id_nombre).val(),
         email: user.email,
         password: $('#' + id_password).val(),
-        especialidad: $('#' + id_especialidad).val(),
-        proyecto: $('#' + id_proyecto).val(),
-        credenciales: $('#' + id_credenciales).val(),
+        especialidad: espec,
+        proyecto: $('#' + id_proys + " option:selected").val(),
+        credenciales: creden,
         activo: false,
     }
     firebase.database().ref(""+rama_bd+"/" + user.uid).set(usuario)
