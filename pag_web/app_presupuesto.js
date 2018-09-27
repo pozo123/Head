953a5cd7aa76846e7e2cd466ca03cc0fb230e13d
artbrare <42916052+artbrare@users.noreply.github.com>
2018-09-27 17:40:35 -0500

    Update app_presupuesto.js

--- pag_web/app_presupuesto.js
+++ pag_web/app_presupuesto.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
         document.getElementById(lista_reqs_presupuesto).appendChild(node);  
         reqs.push({nombre:""+selec,entregado: false,esencial: r.esencial,timestamps:{startedAt: time.getTime(), entrega: 0}});//firebase.database.ServerValue.TIMESTAMP pero lo da en milisegundos
         alert(JSON.stringify(reqs));
-     }
+     });
 
  });
 
