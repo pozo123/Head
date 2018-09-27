c2e267cb0adace5b6b06d32ff023cd6acc642899
pozo123 <diego.pozo@hotmail.com>
2018-09-27 13:39:29 -0500

    Update app_inge.js

--- pag_web/app_inge.js
+++ pag_web/app_inge.js
@@ -1,46 +1,39 @@
 // JavaScript source code
-var id_nombre = "ingeNombre";
-var id_email = "ingeEmail";
-var id_password = "ingePwd";
-var id_especialidad = "especialidad";
-var id_proyecto = "proyecto";
-var id_credenciales = "credenciales";
-var id_uid = "uid";
-var id_registrar = "registrarIngeniero";
-var id_imprimir = "imprimir";
-var rama_bd1 = "inges";
-
+var id_nombre_inge = "ingeNombre";
+var id_email_inge = "ingeEmail";
+var id_password_inge = "ingePwd";
+var id_elec_checkbox_inge = "checkboxElectricidad";
+var id_plom_checkbox_inge = "checkboxPlomeria";
+var id_admin_radio_inge = "inlineRadio1";
+var id_lider_radio_inge = "inlineRadio2";
+var id_proyectoAsignado_ddl_inge = "proyectoAsignado";
+var id_proyectista_radio_inge = "inlineRadio3";
+var id_registrar_button_inge = "registrarIngeniero";
+var rama_bd_inges = "inges";
 var rama_bd_proys = "proys";
-var id_proys = "proyectoAsignado";
-var id_elec = "checkboxElectricidad";
-var id_plom = "checkboxPlomeria";
-var id_admin = "inlineRadio1";
-var id_lider = "inlineRadio2";
-var id_proyectista = "inlineRadio3";
+
+//var id_imprimir_button_inge = "imprimir";
+//var id_uid_inge = "uid";
 
 $(document).ready(function(){
-    var select = document.getElementById(id_proys);
+    var select = document.getElementById(id_proyectoAsignado_ddl_inge);
     var option = document.createElement('option');
     option.style = "display:none";
     option.text = option.value = "";
     select.appendChild(option);
     
-    firebase.database().ref('' + rama_bd_proys).orderByChild('nombre').on('child_added',function(snapshot){
-        
+    firebase.database().ref(rama_bd_proys).orderByChild('nombre').on('child_added',function(snapshot){
         var proy = snapshot.val();
         var option2 = document.createElement('option');
         option2.text = option2.value = proy.nombre; 
         select.appendChild(option2);
     });
-    console.log("que pedo")
 });
 
-$('#' + id_registrar).click((function () {
-    alert("hola");
-    firebase.auth().createUserWithEmailAndPassword($('#' + id_email).val(), $('#' + id_password).val())
+$('#' + id_registrar_button_inge).click((function () {
+    firebase.auth().createUserWithEmailAndPassword($('#' + id_email_inge).val(), $('#' + id_password_inge).val())
         .then(function (result) {
             guardaDatos(result.user);
-            $('#' + id_uid).val(result.user.uid);
         });
 
     /*Para imprimir errores, .catch en vez de .then
@@ -58,120 +51,122 @@ $('#' + id_registrar).click((function () {
         });*/
 }));
 
-$('#' + id_imprimir).click((function () {
-    if ($('#' + id_nombre).val() != "") {
-        firebase.database().ref('' + rama_bd1).orderByChild('nombre').equalTo($('#' + id_nombre).val()).on("child_added", function (snapshot) {
-            var user = snapshot.val();
-            //console.log(user.nombre);
-            $('#' + id_email).val(user.email);
-            $('#' + id_password).val(user.password);
-            if(user.especialidad == 1)
-                document.getElementById(id_elec).checked = true;
-            else if(user.especialidad == 2)
-                document.getElementById(id_plom).checked = true;
-            else if(user.especialidad == 3){
-                document.getElementById(id_elec).checked = true;
-                document.getElementById(id_plom).checked = true;
-            }
+// $('#' + id_imprimir_button_inge).click((function () {
+//     if ($('#' + id_nombre_inge).val() != "") {
+//         firebase.database().ref('' + rama_bd_inges).orderByChild('nombre').equalTo($('#' + id_nombre_inge).val()).on("child_added", function (snapshot) {
+//             var user = snapshot.val();
+//             //console.log(user.nombre);
+//             $('#' + id_email_inge).val(user.email);
+//             $('#' + id_password_inge).val(user.password);
+//             if(user.especialidad == 1)
+//                 document.getElementById(id_elec_checkbox_inge).checked = true;
+//             else if(user.especialidad == 2)
+//                 document.getElementById(id_plom_checkbox_inge).checked = true;
+//             else if(user.especialidad == 3){
+//                 document.getElementById(id_elec_checkbox_inge).checked = true;
+//                 document.getElementById(id_plom_checkbox_inge).checked = true;
+//             }
                 
-            //$('#' + id_proyecto).val(user.proyecto);
+//             //$('#' + id_proyectoAsignado_ddl_inge).val(user.proyecto);
             
-            if(user.credenciales == 1)
-                document.getElementById(id_admin).checked = true;
-            else if(user.credenciales == 2)
-                document.getElementById(id_lider).checked = true;
-            else if(user.credenciales == 3)
-                document.getElementById(id_proyectista).checked = true;
+//             if(user.credenciales == 1)
+//                 document.getElementById(id_admin_radio_inge).checked = true;
+//             else if(user.credenciales == 2)
+//                 document.getElementById(id_lider_radio_inge).checked = true;
+//             else if(user.credenciales == 3)
+//                 document.getElementById(id_proyectista_radio_inge).checked = true;
                 
-            $('#' + id_uid).val(user.uid);
-        })
-    }
-    else if ($('#' + id_email).val() != "") {
-        firebase.database().ref('' + rama_bd1).orderByChild('email').equalTo($('#' + id_email).val()).on("child_added", function (snapshot) {
-            var user = snapshot.val();
-            //console.log(user.nombre);
-            $('#' + id_nombre).val(user.nombre);
-            $('#' + id_password).val(user.password);
-            if(user.especialidad == 1)
-                document.getElementById(id_elec).checked = true;
-            else if(user.especialidad == 2)
-                document.getElementById(id_plom).checked = true;
-            else if(user.especialidad == 3){
-                document.getElementById(id_elec).checked = true;
-                document.getElementById(id_plom).checked = true;
-            }
+//             $('#' + id_uid_inge).val(user.uid);
+//         })
+//     }
+//     else if ($('#' + id_email_inge).val() != "") {
+//         firebase.database().ref('' + rama_bd_inges).orderByChild('email').equalTo($('#' + id_email_inge).val()).on("child_added", function (snapshot) {
+//             var user = snapshot.val();
+//             //console.log(user.nombre);
+//             $('#' + id_nombre_inge).val(user.nombre);
+//             $('#' + id_password_inge).val(user.password);
+//             if(user.especialidad == 1)
+//                 document.getElementById(id_elec_checkbox_inge).checked = true;
+//             else if(user.especialidad == 2)
+//                 document.getElementById(id_plom_checkbox_inge).checked = true;
+//             else if(user.especialidad == 3){
+//                 document.getElementById(id_elec_checkbox_inge).checked = true;
+//                 document.getElementById(id_plom_checkbox_inge).checked = true;
+//             }
                 
-            //$('#' + id_proyecto).val(user.proyecto);
+//             //$('#' + id_proyectoAsignado_ddl_inge).val(user.proyecto);
             
-            if(user.credenciales == 1)
-                document.getElementById(id_admin).checked = true;
-            else if(user.credenciales == 2)
-                document.getElementById(id_lider).checked = true;
-            else if(user.credenciales == 3)
-                document.getElementById(id_proyectista).checked = true;
+//             if(user.credenciales == 1)
+//                 document.getElementById(id_admin_radio_inge).checked = true;
+//             else if(user.credenciales == 2)
+//                 document.getElementById(id_lider_radio_inge).checked = true;
+//             else if(user.credenciales == 3)
+//                 document.getElementById(id_proyectista_radio_inge).checked = true;
             
-            $('#' + id_uid).val(user.uid);
-        })
-    }
-    else if ($('#' + id_uid).val() != "") {
-        firebase.database().ref('' + rama_bd1).orderByChild('uid').equalTo($('#' + id_uid).val()).on("child_added", function (snapshot) {
-            var user = snapshot.val();
-            //console.log(user.nombre);
-            $('#' + id_nombre).val(user.nombre);
-            $('#' + id_email).val(user.email);
-            $('#' + id_password).val(user.password);
-            if(user.especialidad == 1)
-                document.getElementById(id_elec).checked = true;
-            else if(user.especialidad == 2)
-                document.getElementById(id_plom).checked = true;
-            else if(user.especialidad == 3){
-                document.getElementById(id_elec).checked = true;
-                document.getElementById(id_plom).checked = true;
-            }
+//             $('#' + id_uid_inge).val(user.uid);
+//         })
+//     }
+//     else if ($('#' + id_uid_inge).val() != "") {
+//         firebase.database().ref('' + rama_bd_inges).orderByChild('uid').equalTo($('#' + id_uid_inge).val()).on("child_added", function (snapshot) {
+//             var user = snapshot.val();
+//             //console.log(user.nombre);
+//             $('#' + id_nombre_inge).val(user.nombre);
+//             $('#' + id_email_inge).val(user.email);
+//             $('#' + id_password_inge).val(user.password);
+//             if(user.especialidad == 1)
+//                 document.getElementById(id_elec_checkbox_inge).checked = true;
+//             else if(user.especialidad == 2)
+//                 document.getElementById(id_plom_checkbox_inge).checked = true;
+//             else if(user.especialidad == 3){
+//                 document.getElementById(id_elec_checkbox_inge).checked = true;
+//                 document.getElementById(id_plom_checkbox_inge).checked = true;
+//             }
                 
-            //$('#' + id_proyecto).val(user.proyecto);
+//             //$('#' + id_proyectoAsignado_ddl_inge).val(user.proyecto);
             
-            if(user.credenciales == 1)
-                document.getElementById(id_admin).checked = true;
-            else if(user.credenciales == 2)
-                document.getElementById(id_lider).checked = true;
-            else if(user.credenciales == 3)
-                document.getElementById(id_proyectista).checked = true;
-        })
-    }
-}))
+//             if(user.credenciales == 1)
+//                 document.getElementById(id_admin_radio_inge).checked = true;
+//             else if(user.credenciales == 2)
+//                 document.getElementById(id_lider_radio_inge).checked = true;
+//             else if(user.credenciales == 3)
+//                 document.getElementById(id_proyectista_radio_inge).checked = true;
+//         })
+//     }
+// }))
 
 function guardaDatos(user) {
     var espec;
     var creden;
-    if(document.getElementById(id_elec).checked == true){
-        if(document.getElementById(id_plom).checked == true){
+    if(document.getElementById(id_elec_checkbox_inge).checked == true){
+        if(document.getElementById(id_plom_checkbox_inge).checked == true){
             espec = 3; //especialidad ambas
         }
         else
             espec = 1; //especialidad elec
     }
-    else if(document.getElementById(id_plom).checked == true)
+    else if(document.getElementById(id_plom_checkbox_inge).checked == true)
         espec = 2; //especialidad plom
-    
-    if(document.getElementById(id_admin).checked == true)
+    else espec = -1; //no ingresada
+
+    if(document.getElementById(id_admin_radio_inge).checked == true)
         creden = 1;
-    else if(document.getElementById(id_lider).checked == true)
+    else if(document.getElementById(id_lider_radio_inge).checked == true)
         creden = 2;
-    else if(document.getElementById(id_proyectista).checked == true)
+    else if(document.getElementById(id_proyectista_radio_inge).checked == true)
         creden = 3;
+    else creden = 4; //No ingresada
     
     var usuario = {
         uid: user.uid,
-        nombre: $('#' + id_nombre).val(),
+        nombre: $('#' + id_nombre_inge).val(),
         email: user.email,
-        password: $('#' + id_password).val(),
+        password: $('#' + id_password_inge).val(),
         especialidad: espec,
-        proyecto: $('#' + id_proys + " option:selected").val(),
+        proyecto_asignado: $('#' + id_proyectoAsignado_ddl_inge + " option:selected").val(),
         credenciales: creden,
-        activo: false,
+        status: false,
+        //Imagen
     }
-    console.log(rama_bd1);
 
-    firebase.database().ref(rama_bd1+"inges/" + user.uid).set(usuario);
+    firebase.database().ref(rama_bd_inges+"/" + user.uid).set(usuario);
 }
