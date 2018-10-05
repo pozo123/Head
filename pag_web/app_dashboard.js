4d24c3e2831640aee630f4af19bb2006d94a1908
pozo123 <diego.pozo@hotmail.com>
2018-10-05 17:08:07 -0500

    Update app_dashboard.js

--- pag_web/app_dashboard.js
+++ pag_web/app_dashboard.js
@@ -1,36 +1,51 @@
 // JavaScript source code
 
+var rama_bd_inges = "inges";
+var rama_bd_registros = "registros";
+
 $(document).ready(function(){
-
+    firebas.database().ref(rama_bd_inges).orderByChild("nombre").on("child_added",function(snapshot){
+       var inge = snapshot.val();
+       var card = document.createElement('div');
+       card.id = "card_" + inge.uid;
+       if(inge.status){
+        firebase.database().ref(rama_bd_registros).orderByChild("inge").equalTo(inge.nombre).once("child_added_", function(snapshot){
+            var reg = snapshot.val();
+            if(reg.status === false){
+                card.className = "card borded-success mb-3";
+                card.setAttribute("style", "max-width: 18rem;");
+                var header = document.createElement('div');
+                header.className = "card-header";
+                header.innerHTML = inge.nombre;
+                var body = document.createElement('div');
+                body.className = "card-body text-success";
+                var p_obra = document.createElement("p");
+                var p_presupuesto = document.createElement("p");
+                var node_obra = document.createTextNode("" + reg.obra);
+                var node_presupuesto = document.createTextNode("" + reg.presupuesto);
+                p_obra.appendChild(node_obra);
+                p_presupuesto.appendChild(node_presupuesto);
+                body.appendChild(p_obra);
+                body.appendChild(p_presupuesto);
+                header.appendChild(body);
+                card.appendChild(header);
+                $('#' + id_card_column_dashboard).appendChild(card);
+            }
+        });
+       }else{
+           card.className = "card border-danger mb-3";
+           card.setAttribute("style", "max-width: 18rem;");
+           var header = document.createElement('div');
+           header.className = "card-header";
+           header.innerHTML = inge.nombre;
+           var body = document.createElement('div');
+           body.className = "card-body text-danger";
+           header.appendChild(body);
+           card.appendChild(header);
+           $('#' + id_card_column_dashboard).appendChild(card);
+       }
+       
+       
+       
+    });
 });
-
-function dashCards() {
-    
-}
-
-{/* 
-    
-
-    CARD VERDE
-    
-    <div class="card border-success mb-3" style="max-width: 18rem;">
-  <div class="card-header">Nombre Ingeniero</div>
-  <div class="card-body text-success">
-
-    <img class="card-img-top" src="..." alt="Card image cap">
-    
-    <p class="card-text">OBRA</p>
-    <p class="card-text">PROYECTO</p>
-    
-  </div>
-</div>
-
-    CARD ROJA
-
-<div class="card border-danger mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body text-danger">
-    <h5 class="card-title">Danger card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div> */}
