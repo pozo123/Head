5f324f520901cc14bebd2ad8e875274a1f8277ea
pozo123 <diego.pozo@hotmail.com>
2018-10-04 20:12:48 -0500

    Update app_presupuesto.js

--- pag_web/app_presupuesto.js
+++ pag_web/app_presupuesto.js
@@ -1,8 +1,4 @@
 var id_nombre_presupuesto = "presupuestoNombre";
-var id_reqs_ddl_presupuesto = "reqs";
-var id_add_reqs_button_presupuesto = "add_reqs";
-var id_del_reqs_button_presupuesto = "borrar_reqs_asignados";
-var lista_reqs_presupuesto = "ListaReqs";
 var id_horas_programadas_presupuesto = "horasProgramadas";
 var id_registrar_button_presupuesto = "registrarPresupuesto";
 var id_obra_ddl_presupuesto = "obraPresupuesto";
@@ -10,27 +6,21 @@ var id_tipo_presupuesto_ddl_presupuesto = "DDLtipoPresupuesto"; //Nueva variable
 var id_precio_presupuesto = "precioPresupuesto";
 var id_precio_sugerido_label_presupuesto = "labelCash";
 
+var id_reqs_ddlcheck_presupuesto = "reqs";
+var id_exclusiones_ddl_check_presupuesto = "exc";
+
 var rama_bd_tipos_presupuesto = "tipos_presupuesto";
 var rama_bd_obras = "obras";
 var rama_bd_reqs = "reqs";
-
-//Necesaria si son checkboxes
-//var num_max_req = "3";
+var rama_bd_exclusiones = "exclusiones";
 
 var precio_hora = 2000;
 $('#' + id_horas_programadas_presupuesto).change(function(){
     $('#' + id_precio_sugerido_label_presupuesto).text(""+($('#' + id_horas_programadas_presupuesto).val() * precio_hora));
 });
 
-var reqs = [];
 
 $(document).ready(function() {
-
-    var select = document.getElementById(id_reqs_ddl_presupuesto);   
-    var option = document.createElement('option');
-    option.style = "display:none";
-    option.text = option.value = "";
-    select.appendChild(option);
 
     var select2 = document.getElementById(id_obra_ddl_presupuesto);   
     var option2 = document.createElement('option');
@@ -44,13 +34,26 @@ $(document).ready(function() {
     option5.text = option5.value = "";
     select3.appendChild(option5);
     
+    var myData = [];
     firebase.database().ref(rama_bd_reqs).orderByChild('nombre').on('child_added',function(snapshot){
-        
         var req = snapshot.val();
-        var option3 = document.createElement('OPTION');
-        option3.text = option3.value = req.nombre; 
-        select.appendChild(option3);
+        myData.push({id: req.nombre, label: req.nombre});
+        $('#' + id_reqs_ddlcheck_presupuesto).dropdownCheckbox("reset");
+        $('#' + id_reqs_ddlcheck_presupuesto).dropdownCheckbox({
+            data: myData,
+            title: "Requisitos"
+        });
+    });
 
+    var myData2 = [];
+    firebase.database().ref(rama_bd_exclusiones).orderByChild('nombre').on('child_added',function(snapshot){
+        var exc = snapshot.val();
+        myData2.push({id: exc.nombre, label: exc.texto});
+        $('#' + id_exclusiones_ddl_check_presupuesto).dropdownCheckbox("reset");
+        $('#' + id_exclusiones_ddl_check_presupuesto).dropdownCheckbox({
+            data: myData2,
+            title: "Exclusiones"
+        });
     });
 
     firebase.database().ref(rama_bd_obras).orderByChild('nombre').on('child_added',function(snapshot){
@@ -73,52 +76,9 @@ $(document).ready(function() {
     });
 });
 
-//JALAR LOS REQS DE BASE DE DATOS Y GENERA CHECKBOXES.
-//los pone al final de la pagina, hay que ver como ponerlos en donde toca (cambiar appendChild por otro?)
-    // var num_reqs = 0;
-
-    // firebase.database().ref('' + rama_bd_reqs).orderByChild('nombre').on('child_added',function(snapshot){
-    //     num_reqs++;
-    //     var req = snapshot.val();
-    //     var checkbox = document.createElement('input');
-    //         checkbox.type = "checkbox";
-    //         checkbox.id = "req" + num_reqs;
-    //         checkbox.name = req.nombre;
-    //         document.body.appendChild(checkbox);
-    
-    //         document.body.appendChild(document.createTextNode(req.nombre));
-    // });
-
-
- $('#' + id_add_reqs_button_presupuesto).click(function () {
-     var selec = $("#" + id_reqs_ddl_presupuesto + " option:selected").val();
-     var node = document.createElement("LI");
-     firebase.database().ref(rama_bd_reqs).orderByChild('nombre').equalTo(selec).on("child_added", function (snapshot) {
-        var r = snapshot.val();
-        node.classList.add("list-group-item");                 // Create a <li> node
-        var textnode = document.createTextNode(selec);        // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
-        document.getElementById(lista_reqs_presupuesto).appendChild(node);  
-        reqs.push({nombre:""+selec,entregado: false,esencial: r.esencial,timestamps:{startedAt: new Date().getTime(), entrega: 0}});//firebase.database.ServerValue.TIMESTAMP pero lo da en milisegundos
-        alert(JSON.stringify(reqs));
-     });
-
- });
-
- //Metodo para borrar el ultimo requisito asignado
- $('#' + id_del_reqs_button_presupuesto).click(function () {
-     var list = document.getElementById(lista_reqs_presupuesto);   // Get the <ul> element with id="myList"
-     list.removeChild(list.lastChild);
-     reqs.pop(); 
- });
 
 $('#' + id_registrar_button_presupuesto).click(function () {
-    // JALA VALORES SELECCIONADOS DE CHECKBOXES SI SON REQS
-    // var reqs = [];
-    // for(var i = 1;i<=num_max_req;i++){
-    //         if(document.getElementById('req'+i).checked == true)
-    //             reqs.push(document.getElementById('req'+i).name)
-    //     }
+
     firebase.database().ref(rama_bd_obras + "/" + $('#' + id_obra_ddl_presupuesto + " option:selected").val()).once('value').then(function(snapshot){
         var obra_selec = snapshot.val();
         var codigo_obra = obra_selec.clave;
@@ -139,7 +99,9 @@ $('#' + id_registrar_button_presupuesto).click(function () {
                 activacion: 0,
                 reqs_completados: 0
             },
-            contrato: false
+            contrato: false,
+            reqs: $('#' + id_reqs_ddlcheck_presupuesto).dropdownCheckbox("checked"),
+            exclusiones: $('#' + id_exclusiones_ddl_check_presupuesto).dropdownCheckbox("checked")
         }
         reqs = [];
         firebase.database().ref(rama_bd_obras + "/" + obra_selec.nombre + "/presupuestos/" + $('#' + id_nombre_presupuesto).val()).set(presupuesto)
