9704e669f46a532a0587f48f175162606f6a9090
artbrare <42916052+artbrare@users.noreply.github.com>
2018-10-05 13:53:55 -0500

    Add files via upload

--- pag_web/app_dashboard.js
+++ pag_web/app_dashboard.js
@@ -7,3 +7,30 @@ $(document).ready(function(){
 function dashCards() {
     
 }
+
+{/* 
+    
+
+    CARD VERDE
+    
+    <div class="card border-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">Nombre Ingeniero</div>
+  <div class="card-body text-success">
+
+    <img class="card-img-top" src="..." alt="Card image cap">
+    
+    <p class="card-text">OBRA</p>
+    <p class="card-text">PROYECTO</p>
+    
+  </div>
+</div>
+
+    CARD ROJA
+
+<div class="card border-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body text-danger">
+    <h5 class="card-title">Danger card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div> */}
