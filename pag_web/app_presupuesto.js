afe73c724a51b5f1cadf7b76b32da041610c0344
pozo123 <diego.pozo@hotmail.com>
2018-10-04 20:21:06 -0500

    Update app_presupuesto.js

--- pag_web/app_presupuesto.js
+++ pag_web/app_presupuesto.js
@@ -13,6 +13,7 @@ var rama_bd_tipos_presupuesto = "tipos_presupuesto";
 var rama_bd_obras = "obras";
 var rama_bd_reqs = "reqs";
 var rama_bd_exclusiones = "exclusiones";
+var rama_bd_clientes = "clientes";
 
 var precio_hora = 2000;
 $('#' + id_horas_programadas_presupuesto).change(function(){
@@ -81,43 +82,46 @@ $('#' + id_registrar_button_presupuesto).click(function () {
 
     firebase.database().ref(rama_bd_obras + "/" + $('#' + id_obra_ddl_presupuesto + " option:selected").val()).once('value').then(function(snapshot){
         var obra_selec = snapshot.val();
-        var codigo_obra = obra_selec.clave;
-        var codigo_cliente = obra_selec.cliente.substring(0,3);
-        var codigo_tipo_proyecto = $('#' + id_tipo_presupuesto_ddl_presupuesto + " option:selected").val();
-        var consecutivo = 0; //Falta programar este pedo, esta denso
-        var clave_presu = codigo_obra + "/" + codigo_cliente + "/" + codigo_tipo_proyecto + consecutivo;
-        var fecha = new Date().getTime();
-        var presupuesto = {      
-            nombre: $('#' + id_nombre_presupuesto).val(),
-            clave: clave_presu,
-            requisitos: reqs,
-            horas_programadas: $('#' + id_horas_programadas_presupuesto).val(),
-            cash_presupuestado: $('#' + id_precio_presupuesto).val(),
-            timestamps: {
-                startedAt: new Date().getTime(),
-                finishedAt: 0,
-                activacion: 0,
-                reqs_completados: 0
-            },
-            contrato: false,
-            reqs: $('#' + id_reqs_ddlcheck_presupuesto).dropdownCheckbox("checked"),
-            exclusiones: $('#' + id_exclusiones_ddl_check_presupuesto).dropdownCheckbox("checked")
-        }
-        reqs = [];
-        firebase.database().ref(rama_bd_obras + "/" + obra_selec.nombre + "/presupuestos/" + $('#' + id_nombre_presupuesto).val()).set(presupuesto)
+        firebase.database().ref(rama_bd_clientes).orderByChild("nombre").equalTo(obra_selec.cliente).once('value').then(function(snapshot){
+            var cliente_selec = snapshot.val();
+            var codigo_obra = obra_selec.clave;
+            var codigo_cliente = cliente_selec.clave;
+            var codigo_tipo_proyecto = $('#' + id_tipo_presupuesto_ddl_presupuesto + " option:selected").val();
+            var consecutivo = 0; //Falta programar este pedo, esta denso
+            var clave_presu = codigo_obra + "/" + codigo_cliente + "/" + codigo_tipo_proyecto + consecutivo;
+            var fecha = new Date().getTime();
+            var presupuesto = {      
+                nombre: $('#' + id_nombre_presupuesto).val(),
+                clave: clave_presu,
+                requisitos: reqs,
+                horas_programadas: $('#' + id_horas_programadas_presupuesto).val(),
+                cash_presupuestado: $('#' + id_precio_presupuesto).val(),
+                timestamps: {
+                    startedAt: new Date().getTime(),
+                    finishedAt: 0,
+                    activacion: 0,
+                    reqs_completados: 0
+                },
+                contrato: false,
+                reqs: $('#' + id_reqs_ddlcheck_presupuesto).dropdownCheckbox("checked"),
+                exclusiones: $('#' + id_exclusiones_ddl_check_presupuesto).dropdownCheckbox("checked")
+            }
+            reqs = [];
+            firebase.database().ref(rama_bd_obras + "/" + obra_selec.nombre + "/presupuestos/" + $('#' + id_nombre_presupuesto).val()).set(presupuesto)
 
-        alert("¡Alta de presupuesto exitosa!");
+            alert("¡Alta de presupuesto exitosa!");
 
-        //Aqui se genera el pdf... creo
-        // Variables: 
-        // fecha (falta darle el formato adecuado)
-        // obra_selec.nombre
-        // obra_selec.direccion (o quieren la direccion del cliente?)
-        // obra_selec.cliente
-        // clave_presu
-        // $('#' + id_tipo_presupuesto_ddl_presupuesto + " option:selected").text() (text jala? el value es el codigo)
-        // AT'N hay que jalarlo de un DDL dependiendo de la obra seleccionada, que se puedan añadir como si fueran reqs
-        // $('#' + id_precio_presupuesto).val()
+            //Aqui se genera el pdf... creo
+            // Variables: 
+            // fecha (falta darle el formato adecuado)
+            // obra_selec.nombre
+            // obra_selec.direccion (o quieren la direccion del cliente?)
+            // obra_selec.cliente
+            // clave_presu
+            // $('#' + id_tipo_presupuesto_ddl_presupuesto + " option:selected").text() (text jala? el value es el codigo)
+            // AT'N hay que jalarlo de un DDL dependiendo de la obra seleccionada, que se puedan añadir como si fueran reqs
+            // $('#' + id_precio_presupuesto).val()
+        });            
     });
 
     
