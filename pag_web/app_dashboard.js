2401235f62130014633156b1eedc3939d993d8ac
pozo123 <diego.pozo@hotmail.com>
2018-10-07 10:28:34 -0500

    Update app_dashboard.js

--- pag_web/app_dashboard.js
+++ pag_web/app_dashboard.js
@@ -4,7 +4,8 @@ var rama_bd_inges = "inges";
 var rama_bd_registros = "registros";
 
 $(document).ready(function(){
-    firebas.database().ref(rama_bd_inges).orderByChild("nombre").on("child_added",function(snapshot){
+    //firebas.database().ref(rama_bd_inges).orderByChild("nombre").on("child_added",function(snapshot){
+    firebas.database().ref(rama_bd_inges).orderByChild("nombre").on("value",function(snapshot){
        var inge = snapshot.val();
        var card = document.createElement('div');
        card.id = "card_" + inge.uid;
