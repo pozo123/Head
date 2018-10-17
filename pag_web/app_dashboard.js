f5ea0de485ed979cce27018e3217baad9171410d
pozo123 <diego.pozo@hotmail.com>
2018-10-17 13:59:24 -0500

    Update app_dashboard.js

--- pag_web/app_dashboard.js
+++ pag_web/app_dashboard.js
@@ -18,55 +18,56 @@ function loadDashcards(){
       var inge = snapshot.val();
       var card = document.createElement('div');
       card.id = "card_" + inge.uid;
-      if(inge.status === true){
-            //alert(inge.nombre + " " + "ingeStatus" + " " + inge.status)
+      if(inge.permisos.perfil === true){
+        if(inge.status === true){
+              //alert(inge.nombre + " " + "ingeStatus" + " " + inge.status)
             firebase.database().ref(rama_bd_registros).orderByChild("inge").equalTo(inge.nombre).on("child_added",function(snapshot){
-            var reg = snapshot.val();
-            if(reg.status === false && flag === true){
-                flag = false;
-                card.className = "card border-success mb-3";
-                card.setAttribute("style", "max-width: 18rem;");
-                var header = document.createElement('div');
-                header.className = "card-header";
-                header.innerHTML = inge.nombre;
-                var body = document.createElement('div');
-                body.className = "card-body text-success";
-                var p_obra = document.createElement("h6");
-                var p_presupuesto = document.createElement("h6");
-                var p_checkin = document.createElement("h6");
-                var node_obra = document.createTextNode("Obra: " + reg.obra);
-                var node_presupuesto = document.createTextNode("Presupuesto: " + reg.presupuesto);
-                var d = new Date(reg.checkin);
-                //alert(reg.checkin);
-                var node_checkin = document.createTextNode("Hora de Inicio: " + d.toLocaleTimeString());
-                p_obra.appendChild(node_obra);
-                p_presupuesto.appendChild(node_presupuesto);
-                p_checkin.appendChild(node_checkin);
-                body.appendChild(p_obra);
-                body.appendChild(p_presupuesto);
-                body.appendChild(p_checkin);
-                header.appendChild(body);
-                card.appendChild(header);
-                var div = document.getElementById(id_card_column_dashboard);
-                div.appendChild(card);
-            }
-        });
-        }else{
-            card.className = "card border-danger mb-3";
-            card.setAttribute("style", "max-width: 18rem;");
-            var header = document.createElement('div');
-            header.className = "card-header";
-            header.innerHTML = inge.nombre;
-            var body = document.createElement('div');
-            body.className = "card-body text-danger";
-            var node = document.createTextNode("No activo");
-            body.appendChild(node);
-            header.appendChild(body);
-            card.appendChild(header);
-            var div = document.getElementById(id_card_column_dashboard);
-            div.appendChild(card);
+              var reg = snapshot.val();
+              if(reg.status === false && flag === true){
+                  flag = false;
+                  card.className = "card border-success mb-3";
+                  card.setAttribute("style", "max-width: 18rem;");
+                  var header = document.createElement('div');
+                  header.className = "card-header";
+                  header.innerHTML = inge.nombre;
+                  var body = document.createElement('div');
+                  body.className = "card-body text-success";
+                  var p_obra = document.createElement("h6");
+                  var p_presupuesto = document.createElement("h6");
+                  var p_checkin = document.createElement("h6");
+                  var node_obra = document.createTextNode("Obra: " + reg.obra);
+                  var node_presupuesto = document.createTextNode("Presupuesto: " + reg.presupuesto);
+                  var d = new Date(reg.checkin);
+                  //alert(reg.checkin);
+                  var node_checkin = document.createTextNode("Hora de Inicio: " + d.toLocaleTimeString());
+                  p_obra.appendChild(node_obra);
+                  p_presupuesto.appendChild(node_presupuesto);
+                  p_checkin.appendChild(node_checkin);
+                  body.appendChild(p_obra);
+                  body.appendChild(p_presupuesto);
+                  body.appendChild(p_checkin);
+                  header.appendChild(body);
+                  card.appendChild(header);
+                  var div = document.getElementById(id_card_column_dashboard);
+                  div.appendChild(card);
+              }
+          });
+          }else{
+              card.className = "card border-danger mb-3";
+              card.setAttribute("style", "max-width: 18rem;");
+              var header = document.createElement('div');
+              header.className = "card-header";
+              header.innerHTML = inge.nombre;
+              var body = document.createElement('div');
+              body.className = "card-body text-danger";
+              var node = document.createTextNode("No activo");
+              body.appendChild(node);
+              header.appendChild(body);
+              card.appendChild(header);
+              var div = document.getElementById(id_card_column_dashboard);
+              div.appendChild(card);
+          }
         }
-        
         
         
     });
