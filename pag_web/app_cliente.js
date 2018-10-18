c837da989946c82596cb2b14967b5d2495d0ccbb
pozo123 <diego.pozo@hotmail.com>
2018-10-18 12:44:21 -0500

    Update app_cliente.js

--- pag_web/app_cliente.js
+++ pag_web/app_cliente.js
@@ -47,22 +47,25 @@ $('#' + id_add_atencion_button_cliente).click(function () {
  });
 
 $('#' + id_registrar_button_cliente).click(function () {
+    if(!$('#' + id_nombre_cliente).val() || !$('#' + id_clave_cliente).val() || (typeof atencion != "undefined" && atencion != null && atencion.length != null && atencion.length > 0)){
+        alert("Llena todos los campos requeridos");
+    }else{
+        var cliente = {      
+            nombre: $('#' + id_nombre_cliente).val(),
+            clave: $('#' + id_clave_cliente).val(),
+            atencion: atencion,
+            direccion: {
+                calle: $('#' + id_direccion_calle_cliente).val(),
+                numero: $('#' + id_direccion_num_cliente).val(),
+                colonia: $('#' + id_direccion_colonia_cliente).val(),
+                delegacion: $('#' + id_direccion_delegacion_cliente).val(),
+                ciudad: $('#' + id_direccion_ciudad_cliente).val(),
+                cp: $('#' + id_direccion_cp_cliente).val()
+            },
+            telefono: $('#' + id_tel_cliente).val(),
+        }
+        firebase.database().ref(rama_bd_clientes + "/" + $('#' + id_nombre_cliente).val()).set(cliente)
 
-    var cliente = {      
-        nombre: $('#' + id_nombre_cliente).val(),
-        clave: $('#' + id_clave_cliente).val(),
-        atencion: atencion,
-        direccion: {
-            calle: $('#' + id_direccion_calle_cliente).val(),
-            numero: $('#' + id_direccion_num_cliente).val(),
-            colonia: $('#' + id_direccion_colonia_cliente).val(),
-            delegacion: $('#' + id_direccion_delegacion_cliente).val(),
-            ciudad: $('#' + id_direccion_ciudad_cliente).val(),
-            cp: $('#' + id_direccion_cp_cliente).val()
-        },
-        telefono: $('#' + id_tel_cliente).val(),
+        alert("¡Alta exitosa!");
     }
-    firebase.database().ref(rama_bd_clientes + "/" + $('#' + id_nombre_cliente).val()).set(cliente)
-
-    alert("¡Alta de cliente exitosa!");
 });
