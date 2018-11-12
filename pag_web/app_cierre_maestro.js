a91ba964c49d8a691966673dde020e59cbdc4d5a
pozo123 <diego.pozo@hotmail.com>
2018-11-12 12:38:04 -0600

    Update app_cierre_maestro.js

--- pag_web/app_cierre_maestro.js
+++ pag_web/app_cierre_maestro.js
@@ -10,11 +10,12 @@ $('#' + id_cerrar_button_cierre).click(function(){
 			var regis = snapshot.val();
             if(regis.status === false && clicked_cierre === true){
                 var horas_registro = new Date().getTime() - regis.checkin;
-                firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/status").set(true);
+                var status = true;
+                firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/status").set(status);
                 firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/horas").set(horas_registro);
                 firebase.database().ref(rama_bd_inges + "/" + ing.uid + "/obras").on('child_added',function(snapshot){
                 	var obra = snapshot.val();
-        	        firebase.database().ref(rama_bd_inges + "/" + username + "/obras/"  + Object.keys(obra)[0]).orderByChild("presupuesto").equalTo(regis.presupuesto).once("child_added").then(function(snapshot){
+        	        firebase.database().ref(rama_bd_inges + "/" + username + "/obras/"  + Object.keys(obra)[0]).orderByChild("presupuesto").equalTo(regis.presupuesto)once("child_added").then(function(snapshot){
 	        	        var aux = snapshot.val();
 	                    horas_previas = aux.horas_trabajadas;
 	                    firebase.database().ref(rama_bd_inges + "/" + username + "/obras/"  + Object.keys(obra)[0] + "/" + regis.presupuesto + "/horas_trabajadas").set(horas_registro + horas_previas);
