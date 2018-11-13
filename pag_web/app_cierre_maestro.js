7ce5bf3c3a904df47c55c360f02ac305c06dfa5c
pozo123 <diego.pozo@hotmail.com>
2018-11-13 12:40:55 -0600

    Update app_cierre_maestro.js

--- pag_web/app_cierre_maestro.js
+++ pag_web/app_cierre_maestro.js
@@ -5,30 +5,29 @@ var rama_bd_registros = "registros";
 $('#' + id_cerrar_button_cierre).click(function(){
 	var clicked_cierre = true;
 	firebase.database().ref(rama_bd_inges).orderByChild("status").equalTo(true).on('child_added',function(snapshot){
+		//Entran todos los inges activos
 		var ing = snapshot.val();
-		firebase.database().ref(rama_bd_registros).orderByChild("inge").equalTo(ing.nombre).on('child_added',function(snapshot){
-			var regis = snapshot.val();
-            if(regis.status === false && clicked_cierre === true){
-                var horas_registro = new Date().getTime() - regis.checkin;
-                var status = true;
-                firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/status").set(status);
-				firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/horas").set(horas_registro);
-				var username = ing.uid;
-                firebase.database().ref(rama_bd_inges + "/" + ing.uid + "/obras").on('child_added',function(snapshot){
-					var obra_key = snapshot.key;
-					
-					alert(rama_bd_inges + "/" + username + "/obras/"  + obra_key);
-        	        firebase.database().ref(rama_bd_inges + "/" + username + "/obras/"  + obra_key).orderByChild("presupuesto").equalTo(regis.presupuesto).once("child_added").then(function(snapshot){
-	        	        var aux = snapshot.val();
-						horas_previas = aux.horas_trabajadas;
+		if(clicked_cierre === true){
+			firebase.database().ref(rama_bd_registros).orderByChild("inge").equalTo(ing.nombre).once('child_added').then(function(snapshot){
+				//Entran TODOS los registros del inge activo
+				var regis = snapshot.val();
+        	    if(regis.status === false){
+        	        var horas_registro = new Date().getTime() - regis.checkin;
+        	        var status = true;
+        	        firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/status").set(status);
+					firebase.database().ref(rama_bd_registros + "/" + regis.cu + "/horas").set(horas_registro);
+					var username = ing.uid;
+        	        firebase.database().ref(rama_bd_inges + "/" + ing.uid + "/obras/" + regis.obra).orderByChild("presupuesto").equalTo(regis.presupuesto).once("child_added").then(function(snapshot){
+						var pres = snapshot.val();
 						var stat = false;
-	                    firebase.database().ref(rama_bd_inges + "/" + username + "/obras/"  + obra_key + "/" + regis.presupuesto + "/horas_trabajadas").set(horas_registro + horas_previas);
-	                    firebase.database().ref(rama_bd_inges + "/" + username + "/status").set(stat);
-					});
-                });
-                clicked_cierre = false;
-            }
-		});
-		alert("Sesion de " + ing.nombre + " cerrada.");
+						var horas_tots = pres.horas_trabajadas + horas_registro;
+        	            firebase.database().ref(rama_bd_inges + "/" + username + "/obras/"  + regis.obra + "/" + regis.presupuesto + "/horas_trabajadas").set(horas_tots);
+        	            firebase.database().ref(rama_bd_inges + "/" + username + "/status").set(stat);
+						alert("Sesion de " + ing.nombre + " cerrada.");
+        	        });
+        	    }
+			});
+			clicked_cierre = false;
+		}
 	});
 });
