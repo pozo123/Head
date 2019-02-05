f4a08bd9862fdd63e40ac24075133241c6cec9ac
pozo123 <diego.pozo@hotmail.com>
2019-02-05 11:24:57 -0600

    Create app_destajistas.js

---
+++ produccion/app_destajistas.js
@@ -0,0 +1,17 @@
+var id_nombre_destajista = "destajistaNombre";
+var id_id_destajista = "destajistaId";
+var id_registrar_button_destajista = "registrarDestajista";
+
+var rama_bd_destajista = "produccion/colaboradores/destajistas";
+
+$('#' + id_registrar_button_destajista).click(function () {
+    if(!$('#' + id_nombre_destajista).val()){
+        alert("Llena todos los campos requeridos");
+    } else {
+    	var destajista = {
+    		nombre: $('#' + id_nombre_destajista).val(),
+    		pagos: "",
+    	}
+    	firebase.database().ref(rama_bd_destajista + "/" + )
+    }
+});
