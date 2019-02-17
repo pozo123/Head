a1e9e64e6617ce00c729dac79accb2e5f585565c
pozo123 <diego.pozo@hotmail.com>
2019-02-17 10:49:45 -0600

    Update app_colaboradores_compras.js

--- compras/app_colaboradores_compras.js
+++ compras/app_colaboradores_compras.js
@@ -1,39 +1,35 @@
 // JavaScript source code
-var id_nombre_colaborador = "colaboradorNombre";
-var id_nickname_colaborador = "colaboradorNickname";
-var id_email_colaborador = "colaboradorEmail";
-var id_password_colaborador = "colaboradorPwd";
-var id_telefono_colaborador = "colaboradorTelefono";
-var id_gerente_rb_colaborador = "checkboxGerente";
-var id_supervisor_rb_colaborador = "checkboxSupervisor";
+var id_nombre_colaborador_compras = "colaboradorNombreCompras";
+var id_nickname_colaborador_compras = "colaboradorNicknameCompras";
+var id_email_colaborador_compras = "colaboradorEmailCompras";
+var id_password_colaborador_compras = "colaboradorPwdCompras";
 
-var id_registrar_button_colaborador = "registrarColaborador";
+var id_registrar_button_colaborador_compras = "registrarColaboradorCompras";
 
-var rama_bd_colaboradores = "produccion/colaboradores";
+var rama_bd_colaboradores_compras = "compras/colaboradores";
 var rama_bd_personal = "personal";
 
 $(document).ready(function(){
-    $("#" + id_supervisor_rb_colaborador).prop("checked", true);
 });
 
-$('#' + id_registrar_button_colaborador).click(function () {
-    if(!$('#' + id_nombre_colaborador).val() || !$('#' + id_email_colaborador).val() || !$('#' + id_password_colaborador).val() || !$('#' + id_nickname_colaborador).val()){
+$('#' + id_registrar_button_colaborador_compras).click(function () {
+    if(!$('#' + id_nombre_colaborador_compras).val() || !$('#' + id_email_colaborador_compras).val() || !$('#' + id_password_colaborador_compras).val() || !$('#' + id_nickname_colaborador_compras).val()){
         alert("Llena todos los campos requeridos");
     } else {
-        secondaryApp.auth().createUserWithEmailAndPassword($('#' + id_email_colaborador).val(), $('#' + id_password_colaborador).val())
+        secondaryApp.auth().createUserWithEmailAndPassword($('#' + id_email_colaborador_compras).val(), $('#' + id_password_colaborador_compras).val())
             .then(function (result) {
-                guardaDatosCol(result.user);
-                guardaDatosPersonalProd(result.user, $('#' + id_nombre_colaborador).val(), $('#' + id_nickname_colaborador).val());
+                guardaDatosColCompras(result.user);
+                guardaDatosPersonalCompras(result.user, $('#' + id_nombre_colaborador).val(), $('#' + id_nickname_colaborador).val());
                 secondaryApp.auth().signOut();
             }, function(error){
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 if (errorCode == 'auth/email-already-in-use') {
-                    firebase.database().ref(rama_bd_personal).orderByChild('email').equalTo($('#' + id_email_colaborador).val()).once('value').then(function(snapshot){
+                    firebase.database().ref(rama_bd_personal).orderByChild('email').equalTo($('#' + id_email_colaborador_compras).val()).once('value').then(function(snapshot){
                         var pers = snapshot.val();
                         guardaDatosCol(pers.uid);
                         var tru = true;
-                        firebase.database().ref(rama_bd_personal + "/" + pers.uid + "/areas/produccion").set(tru);
+                        firebase.database().ref(rama_bd_personal + "/" + pers.uid + "/areas/compras").set(tru);
                     });
                 } else {
                     alert(errorMessage);
@@ -42,11 +38,11 @@ $('#' + id_registrar_button_colaborador).click(function () {
     }
 });
 
-function guardaDatosPersonalProd(user, nombre, nickname) {
+function guardaDatosPersonalCompras(user, nombre, nickname) {
     var areas = {
         //proyectos: true,
-        produccion: true,
-        //compras: false,
+        //produccion: true,
+        compras: true,
         //administracion: false,
     }
 
@@ -62,28 +58,14 @@ function guardaDatosPersonalProd(user, nombre, nickname) {
     alert("¡Alta exitosa!")
 }
 
-function guardaDatosCol(user) {
-    var colaborador;
-    if($('#' + id_supervisor_rb_colaborador).is(':checked')){
-        colaborador = {
-            uid: user.uid,
-            nombre: $('#' + id_nombre_colaborador).val(),
-            email: user.email,
-            nickname: $('#' + id_nickname_colaborador).val(),
-            tipo: "supervisor",
-            obras: "",//Chance las llenamos
-        }
-    } else {
-        colaborador = {
-            uid: user.uid,
-            nombre: $('#' + id_nombre_colaborador).val(),
-            email: user.email,
-            nickname: $('#' + id_nickname_colaborador).val(),
-            tipo: "gerente",
-            obras: "NA",
-        }
+function guardaDatosColCompras(user) {
+    var colaborador = {
+        uid: user.uid,
+        nombre: $('#' + id_nombre_colaborador_compras).val(),
+        email: user.email,
+        nickname: $('#' + id_nickname_colaborador_compras).val(),
     }
 
-    firebase.database().ref(rama_bd_colaboradores + "/" + user.uid).set(colaborador);
+    firebase.database().ref(rama_bd_colaboradores_compras + "/" + user.uid).set(colaborador);
     alert("¡Alta exitosa!")
 }
