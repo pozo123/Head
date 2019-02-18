019cac550fd9188a03d15540341b3bd74c558e26
pozo123 <diego.pozo@hotmail.com>
2019-02-18 08:40:22 -0600

    Update app_colaboradores_admin.js

--- administracion/app_colaboradores_admin.js
+++ administracion/app_colaboradores_admin.js
@@ -27,7 +27,7 @@ $('#' + id_registrar_button_colaborador_admin).click(function () {
                 if (errorCode == 'auth/email-already-in-use') {
                     firebase.database().ref(rama_bd_personal).orderByChild('email').equalTo($('#' + id_email_colaborador_admin).val()).once('value').then(function(snapshot){
                         var pers = snapshot.val();
-                        guardaDatosCol(pers.uid);
+                        guardaDatosColAdmin(pers.uid);
                         var tru = true;
                         firebase.database().ref(rama_bd_personal + "/" + pers.uid + "/areas/administracion").set(tru);
                     });
