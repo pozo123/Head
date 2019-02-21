6e1ec8e61e0910d35b7de905e63d746f06d63a13
artbrare <42916052+artbrare@users.noreply.github.com>
2019-02-21 02:20:07 -0600

    Update app_kaizen_global.js

--- produccion/app_kaizen_global.js
+++ produccion/app_kaizen_global.js
@@ -7,66 +7,73 @@
 
 //Por revisar:
 // formato fechas pStart y pEnd
-var id_gantt_div_global = "kaizenGlobalDiv";
-var id_total_oficina_global = "totalOfGlobal";
-var id_total_score_global = "totalScoreGlobal";
-var id_total_profit_global = "totalProfitGlobal";
-var id_total_total_global = "totalTotalGlobal";
-
 var rama_bd_obras_magico = "obras";
-
+var id_gantt_div_global = "kaizenGlobalDiv"
 var css = ["gtaskblue", "gtaskred", "gtaskgreen", "gtaskyellow", "gtaskpurple", "gtaskpink"];
 
 //Pasar a tab
 //var id_tab_kaizen_global = "kaizenGlobalTab";
 //$('#' + id_tab_kaizen_global).click(function(){
 $(document).ready(function(){
-	$('#' + id_total_oficina_global).val(0);
-	$('#' + id_total_score_global).val(0);
-	$('#' + id_total_profit_global).val(0);
-	$('#' + id_total_total_global).val(0);
-	var g = new JSGantt.GanttChart(document.getElementById(id_gantt_div_global), 'month');
+	var g = new JSGantt.GanttChart(document.getElementById(id_gantt_div_global), 'day');
+    g.setOptions({
+        vCaptionType: 'Complete',  // Set to Show Caption : None,Caption,Resource,Duration,Complete,     
+        vQuarterColWidth: 36,
+        vDateTaskDisplayFormat: 'day dd month yyyy', // Shown in tool tip box
+        vDayMajorDateDisplayFormat: 'mon yyyy - Week ww',// Set format to dates in the "Major" header of the "Day" view
+        vWeekMinorDateDisplayFormat: 'dd mon', // Set format to display dates in the "Minor" header of the "Week" view
+        vLang: 'en',
+        vShowTaskInfoLink: 1, // Show link in tool tip (0/1)
+        vShowEndWeekDate: 0,  // Show/Hide the date for the last day of the week in header for daily
+        vAdditionalHeaders: { // Add data columns to your table
+            category: {
+              title: 'Category'
+            },
+            sector: {
+              title: 'Sector'
+            }
+          },
+        vUseSingleCell: 10000, // Set the threshold cell per table row (Helps performance for large data.
+        vFormatArr: ['Day', 'Week', 'Month', 'Quarter'], // Even with setUseSingleCell using Hour format on such a large chart can cause issues in some browsers,
+    });
 	firebase.database().ref(rama_bd_obras_magico).once('value').then(function(snapshot){
-		var i = 0;
+        var i = 1;
 		snapshot.forEach(function(obraSnap){
 			var obra = obraSnap.val();
-			var costo_obra = parseFloat(obra.ADMINISTRACION.ESTIMACIONES.PPTO) + parseFloat(obra.ADMINISTRACION.ANTICIPOS.PPTO);
 			g.AddTaskItemObject({
 				pID: i,
 				pName: obra.nombre,
 				pStart: obra.fechas.fecha_inicio_teorica,
-				pEnd: obras.fechas.fecha_final_teorica,
+				pEnd: obra.fechas.fecha_final_teorica,
 				pPlanStart: "",//fecha_inicio_real? o al reves?
 				pPlanEnd: "",//fecha_final_real? o al reves?
 				pClass: "ggroupblack",
 				pLink: "",
 				pMile: 0,
 				pRes: "",//obra.supervisor,
-				pComp: obra.kaizen.AVANCE.REAL,
+				pComp: "",//obra.kaizen.AVANCE.REAL,
 				pGroup: 1, //0-> no grupo, 1-> grupo
 				pParent: 0, //id parent. 0-> este es el parent
 				pOpen: 0,
 				pDepend: "", //Para flechitas si hay dependencia entre procesos
 				pCaption: "",
 				//AQUI Costo, score, of y profit con valores prog.... no? Cómo lo hacemos?
-				pCost: costo_obra,
+				/*pCost: costo_obra,
 				SCORE: obra.kaizen.PROYECTOS.PPTO,
 				OF: costo_obra*0.2,
-				PROFIT: obra.kaizen.PROFIT.PROG,
-			});
-			$('#' + id_total_oficina_global).val(0) = $('#' + id_total_oficina_global).val() + obra.kaizen.PROYECTOS.PPTO;
-			$('#' + id_total_score_global).val(0) = $('#' + id_total_score_global).val() + costo_obra*0.2;
-			$('#' + id_total_profit_global).val(0) = $('#' + id_total_profit_global).val() + obra.kaizen.PROFIT.PROG;
-			$('#' + id_total_total_global).val(0) = $('#' + id_total_total_global).val() + obra.kaizen.PROYECTOS.PPTO + costo_obra*0.2 + obra.kaizen.PROFIT.PROG;
+				PROFIT: obra.kaizen.PROFIT.PROG,*/
+            });
+            console.log("Obra: " + obra.nombre + " id: " + i);
 			var j = 0;
+            var id_obra = i;
 			obraSnap.child("procesos").forEach(function(childSnapshot){
-				var id_obra = i;
 				i++;
-				var proc = childSnapshot.val();
-				var costo_proc = parseFloat(proc.ADMINISTRACION.ESTIMACIONES.PPTO) + parseFloat(proc.ADMINISTRACION.ANTICIPOS.PPTO);
+                var proc = childSnapshot.val();
+                console.log(proc);
+				//var costo_proc = parseFloat(proc.ADMINISTRACION.ESTIMACIONES.PPTO) + parseFloat(proc.ADMINISTRACION.ANTICIPOS.PPTO);
 				g.AddTaskItemObject({
 					pID: i,
-					pName: proc.nombre,
+					pName: proc.clave,
 					pStart: proc.fechas.fecha_inicio,
 					pEnd: proc.fechas.fecha_final,
 					pPlanStart: "",//fecha_inicio_real? o al reves?
@@ -75,64 +82,65 @@ $(document).ready(function(){
 					pLink: "",
 					pMile: 0,
 					pRes: "",//obra.supervisor,
-					pComp: proc.kaizen.AVANCE.REAL,
+					pComp: "",//proc.kaizen.AVANCE.REAL,
 					pGroup: 1, //0-> no grupo, 1-> grupo
 					pParent: id_obra, //id parent. 0-> este es el parent
 					pOpen: 0,
 					pDepend: "", //Para flechitas si hay dependencia entre procesos
 					pCaption: "",
 					//AQUI Costo, score, of y profit con valores prog.... no? Cómo lo hacemos?
-					pCost: costo_proc,
+					pCost: "",//costo_proc,
 					SCORE: proc.kaizen.PROYECTOS.PPTO,
-					OF: costo_proc*0.2,
+					OF: "",//costo_proc*0.2,
 					PROFIT: proc.kaizen.PROFIT.PROG,
-				});
-				if(proc.subprocesos != ""){
-					var k = 0;
-					childSnapshot.child("subprocesos").forEach(function(subProcSnap){
-						var id_proc = i;
-						i++;
-						var subproc = subProcSnap.val();
-						var costo_subproc = parseFloat(subproc.ADMINISTRACION.ESTIMACIONES.PPTO) + parseFloat(subproc.ADMINISTRACION.ANTICIPOS.PPTO);
-						g.AddTaskItemObject({
-							pID: i,
-							pName: subproc.nombre,
-							pStart: subproc.fechas.fecha_inicio,
-							pEnd: subproc.fechas.fecha_final,
-							pPlanStart: "",//fecha_inicio_real? o al reves?
-							pPlanEnd: "",//fecha_final_real? o al reves?
-							pClass: css[k%6],
-							pLink: "",
-							pMile: 0,
-							pRes: "",//obra.supervisor,
-							pComp: subproc.kaizen.AVANCE.REAL,
-							pGroup: 1, //0-> no grupo, 1-> grupo
-							pParent: id_proc, //id parent. 0-> este es el parent
-							pOpen: 0,
-							pDepend: "", //Para flechitas si hay dependencia entre procesos
-							pCaption: "",
-							//AQUI Costo, score, of y profit con valores prog.... no? Cómo lo hacemos?
-							pCost: costo_subproc,
-							SCORE: subproc.kaizen.PROYECTOS.PPTO,
-							OF: costo_subproc*0.2,
-							PROFIT: subproc.kaizen.PROFIT.PROG,
-						});
-						k++;
-					});
-				}
-				j++;
+                });
+                console.log("proc: " + proc.clave + " id: " + i);
+                if(proc.subprocesos != ""){
+                    var k = 0;
+                    var id_proc = i;
+                    childSnapshot.child("subprocesos").forEach(function(subProcSnap){
+                        i++;
+                        var subproc = subProcSnap.val();
+                        //var costo_subproc = parseFloat(subproc.ADMINISTRACION.ESTIMACIONES.PPTO) + parseFloat(subproc.ADMINISTRACION.ANTICIPOS.PPTO);
+                        g.AddTaskItemObject({
+                            pID: i,
+                            pName: subproc.clave,
+                            pStart: subproc.fechas.fecha_inicio,
+                            pEnd: subproc.fechas.fecha_final,
+                            pPlanStart: "",//fecha_inicio_real? o al reves?
+                            pPlanEnd: "",//fecha_final_real? o al reves?
+                            pClass: css[k%6],
+                            pLink: "",
+                            pMile: 0,
+                            pRes: "",//obra.supervisor,
+                            pComp: "",//subproc.kaizen.AVANCE.REAL,
+                            pGroup: 1, //0-> no grupo, 1-> grupo
+                            pParent: id_proc, //id parent. 0-> este es el parent
+                            pOpen: 0,
+                            pDepend: "", //Para flechitas si hay dependencia entre procesos
+                            pCaption: "",
+                            //AQUI Costo, score, of y profit con valores prog.... no? Cómo lo hacemos?
+                            pCost: "",//costo_subproc,
+                            SCORE: subproc.kaizen.PROYECTOS.PPTO,
+                            OF: "",//costo_subproc*0.2,
+                            PROFIT: subproc.kaizen.PROFIT.PROG,
+                        });
+                        k++;
+                    });
+                }
+                j++;
+
 			});
 			i++;
-		});
+        });
+        g.Draw();
 	})
-	g.setShowRes(0);
-	g.setShowDur(0);
-	g.setShowComp(0);
-	g.setShowStartDate(0);
-	g.setShowEndDate(0);
-	g.setAdditionalHeaders({ SCORE: { title: 'SCORE' } });//checar si sintaxis sí es así
-	g.setAdditionalHeaders({ OF: { title: 'OF' } });//checar si sintaxis sí es así
-	g.setAdditionalHeaders({ PROFIT: { title: 'PROFIT' } });//checar si sintaxis sí es así
-
-	g.draw();
+	//g.setShowRes(0);
+	//g.setShowDur(0);
+	//g.setShowComp(0);
+	//g.setShowStartDate(0);
+	//g.setShowEndDate(0);
+	//g.setAdditionalHeaders({ SCORE: { title: 'SCORE' } });//checar si sintaxis sí es así
+	//g.setAdditionalHeaders({ OF: { title: 'OF' } });//checar si sintaxis sí es así
+	//g.setAdditionalHeaders({ PROFIT: { title: 'PROFIT' } });//checar si sintaxis sí es así
 });
