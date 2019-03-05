22f12e1be42ff26604f728797c967f7ba912f00e
pozo123 <diego.pozo@hotmail.com>
2019-03-05 16:42:44 -0600

    Update app_colaboradores_admin.js

--- administracion/app_colaboradores_admin.js
+++ administracion/app_colaboradores_admin.js
@@ -1,49 +1,78 @@
 // JavaScript source code
-var id_nombre_colaborador_admin = "colaboradorNombreAdmin";
-var id_nickname_colaborador_admin = "colaboradorNicknameAdmin";
-var id_email_colaborador_admin = "colaboradorEmailAdmin";
-var id_password_colaborador_admin = "colaboradorPwdAdmin";
+var id_nombre_colaborador_rrhh = "colaboradorNombreRRHH";
+var id_nickname_colaborador_rrhh = "colaboradorNicknameRRHH";
+var id_email_colaborador_rrhh = "colaboradorEmailRRHH";
+var id_password_colaborador_rrhh = "colaboradorPwdRRHH";
 
-var id_registrar_button_colaborador_admin = "registrarColaboradorAdmin";
+var id_registrar_button_colaborador_rrhh = "registrarColaboradorRRHH";
 
-var rama_bd_colaboradores_admin = "administracion/colaboradores";
+var rama_bd_colaboradores_rrhh = "rrhh/colaboradores";
 var rama_bd_personal = "personal";
 
 $(document).ready(function(){
 });
 
-$('#' + id_registrar_button_colaborador_admin).click(function () {
-    if(!$('#' + id_nombre_colaborador_admin).val() || !$('#' + id_email_colaborador_admin).val() || !$('#' + id_password_colaborador_admin).val() || !$('#' + id_nickname_colaborador_admin).val()){
+var existe = false;
+$("#" + id_email_colaborador_rrhh).change(function(){
+    existe = false;
+    firebase.database().ref(rama_bd_personal).once('value').then(function(snapshot){
+        var nombre = "";
+        var nickname = "";
+        snapshot.forEach(function(child_snap){
+            var pers = child_snap.val();
+            if(pers.email == $("#" + id_email_colaborador_rrhh).val()){
+                existe = true;
+                nombre = pers.nombre;
+                nickname = pers.nickname;
+            }
+        });
+        if(existe){
+            $('#' + id_nombre_colaborador_rrhh).val(nombre);
+            $('#' + id_nickname_colaborador_rrhh).val(nickname);
+            document.getElementById(id_nombre_colaborador_rrhh).disabled = true;
+            document.getElementById(id_nickname_colaborador_rrhh).disabled = true;
+            document.getElementById(id_password_colaborador_rrhh).disabled = true;
+        } else {
+            if(document.getElementById(id_nombre_colaborador_rrhh).disabled == true){
+                $('#' + id_nombre_colaborador_rrhh).val("");
+                $('#' + id_nickname_colaborador_rrhh).val("");
+            }
+            document.getElementById(id_nombre_colaborador_rrhh).disabled = false;
+            document.getElementById(id_nickname_colaborador_rrhh).disabled = false;
+            document.getElementById(id_password_colaborador_rrhh).disabled = false;
+        }
+    });
+});
+
+$('#' + id_registrar_button_colaborador_rrhh).click(function () {
+    if(!$('#' + id_nombre_colaborador_rrhh).val() || !$('#' + id_email_colaborador_rrhh).val() || (document.getElementById(id_password_colaborador_rrhh).disabled == false && !$('#' + id_password_colaborador_rrhh).val()) || !$('#' + id_nickname_colaborador_rrhh).val()){
         alert("Llena todos los campos requeridos");
     } else {
-        secondaryApp.auth().createUserWithEmailAndPassword($('#' + id_email_colaborador_admin).val(), $('#' + id_password_colaborador_admin).val())
-            .then(function (result) {
-                guardaDatosColAdmin(result.user);
-                guardaDatosPersonalAdmin(result.user, $('#' + id_nombre_colaborador_admin).val(), $('#' + id_nickname_colaborador_admin).val());
-                secondaryApp.auth().signOut();
-            }, function(error){
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                if (errorCode == 'auth/email-already-in-use') {
-                    firebase.database().ref(rama_bd_personal).orderByChild('email').equalTo($('#' + id_email_colaborador_admin).val()).once('value').then(function(snapshot){
-                        var pers = snapshot.val();
-                        guardaDatosColAdmin(pers);
-                        var tru = true;
-                        firebase.database().ref(rama_bd_personal + "/" + pers.uid + "/areas/administracion").set(tru);
-                    });
-                } else {
-                    alert(errorMessage);
-                }
+        if(existe){
+            firebase.database().ref(rama_bd_personal).orderByChild('email').equalTo($('#' + id_email_colaborador_rrhh).val()).once('child_added').then(function(snapshot){
+                var pers = snapshot.val();
+                guardaDatosRRHH(pers);
+                var tru = true;
+                firebase.database().ref(rama_bd_personal + "/" + pers.uid + "/areas/rrhh").set(tru);
             });
+        } else {
+            secondaryApp.auth().createUserWithEmailAndPassword($('#' + id_email_colaborador_rrhh).val(), $('#' + id_password_colaborador_rrhh).val())
+                .then(function (result) {
+                    guardaDatosCol(result.user);
+                    guardaDatosPersonalProd(result.user, $('#' + id_nombre_colaborador_rrhh).val(), $('#' + id_nickname_colaborador_rrhh).val());
+                    secondaryApp.auth().signOut();
+                }
+            );
+        }
     }
 });
 
-function guardaDatosPersonalAdmin(user, nombre, nickname) {
+function guardaDatosPersonalRRHH(user, nombre, nickname) {
     var areas = {
         //proyectos: true,
         //produccion: true,
-        //rrhh: true,
-        administracion: true,
+        rrhh: true,
+        //administracion: false,
     }
 
     var persona = {
@@ -58,14 +87,68 @@ function guardaDatosPersonalAdmin(user, nombre, nickname) {
     alert("¡Alta exitosa!")
 }
 
-function guardaDatosColAdmin(user) {
+function guardaDatosColRRHH(user) {
     var colaborador = {
         uid: user.uid,
-        nombre: $('#' + id_nombre_colaborador_admin).val(),
+        nombre: $('#' + id_nombre_colaborador_rrhh).val(),
         email: user.email,
-        nickname: $('#' + id_nickname_colaborador_admin).val(),
+        nickname: $('#' + id_nickname_colaborador_rrhh).val(),
     }
 
-    firebase.database().ref(rama_bd_colaboradores_admin + "/" + user.uid).set(colaborador);
+    firebase.database().ref(rama_bd_colaboradores_rrhh + "/" + user.uid).set(colaborador);
     alert("¡Alta exitosa!")
-}
+}- app_colaboradores_rrhh- 
+var existe = false;
+$("#" + id_email_colaborador_rrhh).change(function(){
+    existe = false;
+    firebase.database().ref(rama_bd_personal).once('value').then(function(snapshot){
+        var nombre = "";
+        var nickname = "";
+        snapshot.forEach(function(child_snap){
+            var pers = child_snap.val();
+            if(pers.email == $("#" + id_email_colaborador_rrhh).val()){
+                existe = true;
+                nombre = pers.nombre;
+                nickname = pers.nickname;
+            }
+        });
+        if(existe){
+            $('#' + id_nombre_colaborador_rrhh).val(nombre);
+            $('#' + id_nickname_colaborador_rrhh).val(nickname);
+            document.getElementById(id_nombre_colaborador_rrhh).disabled = true;
+            document.getElementById(id_nickname_colaborador_rrhh).disabled = true;
+            document.getElementById(id_password_colaborador_rrhh).disabled = true;
+        } else {
+            if(document.getElementById(id_nombre_colaborador_rrhh).disabled == true){
+                $('#' + id_nombre_colaborador_rrhh).val("");
+                $('#' + id_nickname_colaborador_rrhh).val("");
+            }
+            document.getElementById(id_nombre_colaborador_rrhh).disabled = false;
+            document.getElementById(id_nickname_colaborador_rrhh).disabled = false;
+            document.getElementById(id_password_colaborador_rrhh).disabled = false;
+        }
+    });
+});
+
+$('#' + id_registrar_button_colaborador_rrhh).click(function () {
+    if(!$('#' + id_nombre_colaborador_rrhh).val() || !$('#' + id_email_colaborador_rrhh).val() || (document.getElementById(id_password_colaborador_rrhh).disabled == false && !$('#' + id_password_colaborador_rrhh).val()) || !$('#' + id_nickname_colaborador_rrhh).val()){
+        alert("Llena todos los campos requeridos");
+    } else {
+        if(existe){
+            firebase.database().ref(rama_bd_personal).orderByChild('email').equalTo($('#' + id_email_colaborador_rrhh).val()).once('child_added').then(function(snapshot){
+                var pers = snapshot.val();
+                guardaDatosRRHH(pers);
+                var tru = true;
+                firebase.database().ref(rama_bd_personal + "/" + pers.uid + "/areas/rrhh").set(tru);
+            });
+        } else {
+            secondaryApp.auth().createUserWithEmailAndPassword($('#' + id_email_colaborador_rrhh).val(), $('#' + id_password_colaborador_rrhh).val())
+                .then(function (result) {
+                    guardaDatosCol(result.user);
+                    guardaDatosPersonalProd(result.user, $('#' + id_nombre_colaborador_rrhh).val(), $('#' + id_nickname_colaborador_rrhh).val());
+                    secondaryApp.auth().signOut();
+                }
+            );
+        }
+    }
+});rrhhaAdmin
