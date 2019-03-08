f3a5a2358517b9b00f442f448c80452ecdfabed4
artbrare <42916052+artbrare@users.noreply.github.com>
2019-03-08 08:46:40 -0600

    Update app_proy_cuant_kaizen.js

--- pag_web/app_proy_cuant_kaizen.js
+++ pag_web/app_proy_cuant_kaizen.js
@@ -1,6 +1,13 @@
 var id_obra_ddl_cuant_kaizen = "obraDdlCuant";
+
+var id_group_proc_cuant_kaizen = "groupProcCuant"
 var id_proc_ddl_cuant_kaizen = "procDdlCuant";//TIENEN QUE IR HIDDEN
+
+
+var id_group_subp_cuant_kaizen = "groupSubpCuant"
 var id_subp_ddl_cuant_kaizen = "subpDdlCuant";//TIENEN QUE IR HIDDEN
+
+
 var id_valor_anterior_cuant_kaizen = "anteriorCuant";
 var id_cantidad_cuant_kaizen = "cantidadCuant";
 var id_valor_nuevo_cuant_kaizen = "nuevoCuant";
@@ -35,10 +42,10 @@ $("#" + id_obra_ddl_cuant_kaizen).change(function(){
 	    $('#' + id_valor_anterior_cuant_kaizen).text("$" + formatMoney(obra.kaizen.PRODUCCION.SUMINISTROS.CUANT));
 	    $('#' + id_valor_nuevo_cuant_kaizen).text(obra.kaizen.PRODUCCION.SUMINISTROS.CUANT);
 	    if(obra.num_procesos == 0){
-	    	$('#' + id_proc_ddl_cuant_kaizen).addClass('hidden');
+	    	$('#' + id_group_proc_cuant_kaizen).addClass('hidden');
 	    	caso = "obra";
 	    } else {
-	    	$('#' + id_proc_ddl_cuant_kaizen).removeClass('hidden');
+	    	$('#' + id_group_proc_cuant_kaizen).removeClass('hidden');
 
 		    var select = document.getElementById(id_proc_ddl_cuant_kaizen);
 		    var option = document.createElement('option');
@@ -65,11 +72,11 @@ $("#" + id_proc_ddl_cuant_kaizen).change(function(){
 	    $('#' + id_valor_anterior_cuant_kaizen).text("$" + formatMoney(proc.kaizen.PRODUCCION.SUMINISTROS.CUANT));
 	    $('#' + id_valor_nuevo_cuant_kaizen).text(proc.kaizen.PRODUCCION.SUMINISTROS.CUANT);
 	    if(proc.num_subprocesos == 0){
-	    	$('#' + id_subp_ddl_cuant_kaizen).addClass('hidden');
+	    	$('#' + id_group_subp_cuant_kaizen).addClass('hidden');
 	    	caso = "proc";
 	    } else {
 	    	caso = "subp";
-	    	$('#' + id_subp_ddl_cuant_kaizen).removeClass('hidden');
+	    	$('#' + id_group_subp_cuant_kaizen).removeClass('hidden');
 	    	$('#' + id_subp_ddl_cuant_kaizen).empty();
 		    var select = document.getElementById(id_subp_ddl_cuant_kaizen);
 		    var option = document.createElement('option');
@@ -88,8 +95,8 @@ $("#" + id_proc_ddl_cuant_kaizen).change(function(){
 });
 
 $("#" + id_subp_ddl_cuant_kaizen).change(function(){
-    firebase.database().ref(rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val() + "/procesos/" + $('#' + id_proc_ddl_cuant_kaizen + " option:selected").val() + "/subprocesos/" + $('#' + id_proc_ddl_cuant_kaizen + " option:selected").val()).once('value').then(function(snapshot){
-	    var subp = snapshot.val();
+    firebase.database().ref(rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val() + "/procesos/" + $('#' + id_proc_ddl_cuant_kaizen + " option:selected").val() + "/subprocesos/" + $('#' + id_subp_ddl_cuant_kaizen + " option:selected").val()).once('value').then(function(snapshot){
+        var subp = snapshot.val();
 	    $('#' + id_valor_anterior_cuant_kaizen).text("$" + formatMoney(subp.kaizen.PRODUCCION.SUMINISTROS.CUANT));
 	    $('#' + id_valor_nuevo_cuant_kaizen).text(subp.kaizen.PRODUCCION.SUMINISTROS.CUANT);
     });
@@ -106,9 +113,9 @@ $('#' + id_actualizar_valor_cuant_kaizen).click(function(){
 	if(caso == "obra"){
 		query = rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val();
 	} else if(caso == "proc"){
-		query = rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val() + "/procesos/" + $('#' id_proc_ddl_cuant_kaizen + " option:selected").val();
+		query = rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val() + "/procesos/" + $('#' + id_proc_ddl_cuant_kaizen + " option:selected").val();
 	} else if(caso == "subp"){
-		query = rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val() + "/procesos/" + $('#' id_proc_ddl_cuant_kaizen + " option:selected").val() + "/subprocesos/" + $('#' + id_subp_ddl_cuant_kaizen + " option:selected").val();
+		query = rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_cuant_kaizen + " option:selected").val() + "/procesos/" + $('#' + id_proc_ddl_cuant_kaizen + " option:selected").val() + "/subprocesos/" + $('#' + id_subp_ddl_cuant_kaizen + " option:selected").val();
 	}
 	firebase.database().ref(query + "/kaizen/PRODUCCION/SUMINISTROS/CUANT").set(nuevo);
 	$('#' + id_valor_anterior_cuant_kaizen).text(nuevo);
