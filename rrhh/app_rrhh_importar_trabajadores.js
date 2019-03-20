816352f2e69256ab90960e55315dd3493dd13ba3
artbrare <42916052+artbrare@users.noreply.github.com>
2019-03-20 13:34:12 -0600

    Update app_rrhh_importar_trabajadores.js

--- rrhh/app_rrhh_importar_trabajadores.js
+++ rrhh/app_rrhh_importar_trabajadores.js
@@ -17,57 +17,68 @@ $('#' + id_file_importarTrabajadores).on("change",(function(event) {
 $('#' + id_button_guardar_importarTrabajadores).on("click",function() {
     var reader = new FileReader();
     var result = {};
-
+    var json = {};
     reader.onload = function (e) {
         var data = e.target.result;
         data = new Uint8Array(data);
         var workbook = XLSX.read(data, {type: 'array'});
-        console.log(workbook);
-        var json = {};
+        json = {};
         workbook.SheetNames.forEach(function (sheetName) {
             var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header: 1});
-            if (roa.length) json[sheetName] = roa;
+            if (roa.length) json = roa;
         });
-        console.log(json);
+        var uid_index = 0;
+        var titulos = [];
+        //AQUI hay que definir
+        var niveles = [0,0,0,0,"claves","claves","claves","info_personal","info_personal","info_personal","info_personal",0,"datos_bancarios","datos_bancarios","datos_bancarios",0,0,"tallas","tallas","tallas",0,0];
+        for(key in json[0]){
+            if(json[0][key] == "id_trabajadores") 
+                uid_index = key;
+            titulos[key] = json[0][key];
+        }
+        var resultado = {};
+        console.log(json)
+        for(key in json){
+            if(key > 0){
+                var trabajador = {};
+                console.log(json[key])
+                for(i=0;i<titulos.length;i++){
+                    var n = niveles[i];
+                    var k = titulos[i];
+                    console.log(json[key][i])
+                    if(n != 0){
+                        var path = n.split("/");
+                        var pointer = trabajador;
+                        for(j = 0; j < path.length; j++){
+                            if(pointer[path[j]] == undefined){
+                                pointer[path[j]] = {};
+                            }
+                            pointer = pointer[path[j]];
+                        }
+                        if(!json[key][i]){
+                            pointer[k] = "";
+                        } else {
+                            pointer[k] = json[key][i];
+                        }
+                    } else {
+                        if(!json[key][i]){
+                            trabajador[k] = "";
+                        } else {
+                            trabajador[k] = json[key][i];
+                        }
+                    }
+                }
+                console.log(json[key][uid_index])
+                resultado[json[key][uid_index]] = trabajador;
+            }
+        }
+        console.log(resultado); 
+        firebase.database().ref(rama_bd_trabajadores).update(resultado);
+        
     };
     reader.readAsArrayBuffer(excelSeleccionado);
 
-    var uid_index;
-    var titulos = [];
-    //AQUI hay que definir
-    var niveles = [0,0,0,"claves","claves","claves","info_personal","info_personal","info_personal","info_personal",0,"datos_bancarios","datos_bancarios","datos_bancarios",0,0,0,"tallas","tallas","tallas",0,0];
-    for(key in json[0]){
-        if(json[0][key] == "id_trabajadores") 
-            uid_index = key;
-        titulos[key] = json[0][key];
-    }
-    console.log(titulos);
-    var resultado = {};
-    for(key in json){
-        if(key > 0){
-            var trabajador = {};
-            for(i=0;i<titulos.length;i++){
-                var n = niveles[i];
-                var k = titulos[i];
-                if(n != 0){
-                    var path = n.split("/");
-                    var pointer = trabajador;
-                    for(j = 0; j < path.length; j++){
-                        if(pointer[path[j]] == undefined){
-                            pointer[path[j]] = {};
-                        }
-                        pointer = pointer[path[j]];
-                    }
-                    pointer[k] = json[key][i];
-                } else {
-                    trabajador[k] = json[key][i];
-                }
-            }
-            resultado[json[key][uid_index]] = trabajador;
-        }
-    }
-    console.log(resultado); 
-    firebase.database().ref(rama_bd_trabajadores).update(resultado);
+
 });
 /*
 var json = {
