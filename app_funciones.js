eceeae21c6ee0793c129d1d5bb5960965cb8cb49
pozo123 <diego.pozo@hotmail.com>
2019-03-25 07:26:45 -0600

    pistaDeAuditoria()

--- app_funciones.js
+++ app_funciones.js
@@ -1,7 +1,8 @@
 
 
-//Recibe el nombre de la hora y regresa un json SCORE:{clave_proceso: horas_trabajadas}
 var rama_bd_obras_magico = "obras";
+
+//Recibe el nombre de la hora y regresa un json SCORE:{clave_proceso: horas_trabajadas}
 function loadScoreKaizen(obra){
   var procesos = {};
   firebase.database().ref(rama_bd_obras_magico + "/" + obra).once('value').then(function(snapshot){
@@ -18,6 +19,36 @@ function loadScoreKaizen(obra){
     console.log(procesos);
     //firebase.database().ref(rama_bd_obras_magico + "/" + obra + "/procesos").update(procesos);
   });
+}
+
+function pistaDeAuditoria(){
+  var pda = {
+    uid: firebase.auth().currentUser.uid,
+    timestamp: new Date().getTime(),
+  };
+  return pda;
+}
+
+//Recibe los COSTOS totales de una obra y regresa el precio de venta para obtener cierto VALOR deseado
+//Si el criterio es "porcentaje" (utilidad con respecto al precio de venta) debe ser entre 0 y 1 (30% = 0.3)
+//Si el criterio es "cantidad" se ingresa el monto exacto que se quiere de utilidad
+//Si el criterio es "precioVenta" el valor es el precio al que se va a vender, y se regresa el valor del profit (en %)
+function calculaUtilidad(costos, criterio, valor){
+  var precioVenta;
+  if(criterio == "porcentaje"){
+    precioVenta = costos/(0.8-valor);
+    return precioVenta;
+  } 
+  if(criterio == "cantidad"){
+    precioVenta = (valor + costos)/0.8;
+    return precioVenta;
+  }
+  if(criterio == "precioVenta"){
+    var profitCantidad = 0.8*valor - costos;
+    var profitPorcentaje = profitCantidad/valor;
+    return profitPorcentaje;
+    //return profitCantidad;
+  }
 }
 //input un dia en ms
 //regresa un array [week, year]
