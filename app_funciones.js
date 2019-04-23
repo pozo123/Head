b4960a0796f91c17eab3e8260a407526868d68b0
pozo123 <diego.pozo@hotmail.com>
2019-04-23 16:00:02 -0500

    calculaKaizen y el kaiz va aquí

--- app_funciones.js
+++ app_funciones.js
@@ -1,7 +1,77 @@
 
 
 var rama_bd_obras_magico = "obras";
+var rama_bd_obras = "obras";
 
+function calculaKaizen(obra,proceso,subproceso){
+  firebase.database().ref(rama_bd_obras + "/" + obra).once('value').then(function(snapshot){
+    if(snapshot.exists()){
+      var json_obra = snapshot.val();K
+      if(proceso != undefined){
+        if(json_obra["procesos"][proceso]["num_subprocesos"] == 0){
+          calculaProfitKaiz(json_obra["procesos"][proceso]);
+        } else {
+          if(subproceso != undefined){
+            calculaProfitKaiz(json_obra["procesos"][proceso]["subprocesos"][subproceso]);
+          }
+          sumaValoresKaiz(json_obra["procesos"][proceso],"proceso");
+        }
+      }
+      sumaValoresKaiz(json_obra,"obra");
+      console.log(json_obra);
+      //firebase.database().ref(rama_bd_obras + "/" + obra).update(json_obra);
+    } else { 
+      alert("No existe esa obra");
+    }
+  });
+}
+
+function sumaValoresKaiz(pointer,tipo){
+  var suma_kaiz = kaiz;
+  var child = tipo == "proceso" ? "subprocesos" : "procesos";
+  for(key in pointer[child]){
+    var kaiz_local = pointer["subprocesos"][key]["kaizen"];
+    suma_kaiz["PROYECTOS"]["PPTO"] += parseFloat(kaiz_local["PROYECTOS"]["PPTO"]);
+    suma_kaiz["PROYECTOS"]["PAG"] += parseFloat(kaiz_local["PROYECTOS"]["PAG"]);
+    suma_kaiz["PRODUCCION"]["COPEO"]["PREC"] += parseFloat(kaiz_local["PRODUCCION"]["COPEO"]["PREC"]);
+    suma_kaiz["PRODUCCION"]["COPEO"]["COPEO"] += parseFloat(kaiz_local["PRODUCCION"]["COPEO"]["COPEO"]);
+    suma_kaiz["PRODUCCION"]["COPEO"]["PAG"] += parseFloat(kaiz_local["PRODUCCION"]["COPEO"]["PAG"]);
+    suma_kaiz["PRODUCCION"]["SUMINISTROS"]["CUANT"] += parseFloat(kaiz_local["PRODUCCION"]["SUMINISTROS"]["CUANT"]);
+    suma_kaiz["PRODUCCION"]["SUMINISTROS"]["OdeC"] += parseFloat(kaiz_local["PRODUCCION"]["SUMINISTROS"]["OdeC"]);
+    suma_kaiz["PRODUCCION"]["SUMINISTROS"]["PAG"] += parseFloat(kaiz_local["PRODUCCION"]["SUMINISTROS"]["PAG"]);
+    suma_kaiz["ADMINISTRACION"]["ESTIMACIONES"]["EST"] += parseFloat(kaiz_local["ADMINISTRACION"]["ESTIMACIONES"]["EST"]);
+    suma_kaiz["ADMINISTRACION"]["ESTIMACIONES"]["PAG"] += parseFloat(kaiz_local["ADMINISTRACION"]["ESTIMACIONES"]["PAG"]);
+    suma_kaiz["ADMINISTRACION"]["ESTIMACIONES"]["PPTO"] += parseFloat(kaiz_local["ADMINISTRACION"]["ESTIMACIONES"]["PPTO"]);
+    suma_kaiz["ADMINISTRACION"]["ANTICIPOS"]["PAG"] += parseFloat(kaiz_local["ADMINISTRACION"]["ANTICIPOS"]["PAG"]);
+    suma_kaiz["ADMINISTRACION"]["ANTICIPOS"]["PPTO"] += parseFloat(kaiz_local["ADMINISTRACION"]["ANTICIPOS"]["PPTO"]);
+  }
+  pointer["kaizen"] = suma_kaiz;
+}
+//Recibe snapshot del objeto que tenga un kaizen (proc o subproc). Obra no porque ese se calcula sumando, por lo de prec/copeo y cuant/odec
+function calculaProfitKaiz(pointer){
+  var kaiz_local = pointer["kaizen"];
+  var proy_ppto = parseFloat(kaiz_local["PROYECTOS"]["PPTO"]);
+  var proy_pag = parseFloat(kaiz_local["PROYECTOS"]["PAG"]);
+  var prod_cop_prec = parseFloat(kaiz_local["PRODUCCION"]["COPEO"]["PREC"]);
+  var prod_cop_copeo = parseFloat(kaiz_local["PRODUCCION"]["COPEO"]["COPEO"]);
+  var prod_cop_pag = parseFloat(kaiz_local["PRODUCCION"]["COPEO"]["PAG"]);
+  var prod_sum_cuant = parseFloat(kaiz_local["PRODUCCION"]["SUMINISTROS"]["CUANT"]);
+  var prod_sum_odec = parseFloat(kaiz_local["PRODUCCION"]["SUMINISTROS"]["OdeC"]);
+  var prod_sum_pag = parseFloat(kaiz_local["PRODUCCION"]["SUMINISTROS"]["PAG"]);
+  var admin_est_est = parseFloat(kaiz_local["ADMINISTRACION"]["ESTIMACIONES"]["EST"]);
+  var admin_est_pag = parseFloat(kaiz_local["ADMINISTRACION"]["ESTIMACIONES"]["PAG"]);
+  var admin_est_ppto = parseFloat(kaiz_local["ADMINISTRACION"]["ESTIMACIONES"]["PPTO"]);
+  var admin_ant_pag = parseFloat(kaiz_local["ADMINISTRACION"]["ANTICIPOS"]["PAG"]);
+  var admin_ant_ppto = parseFloat(kaiz_local["ADMINISTRACION"]["ANTICIPOS"]["PPTO"]);
+
+  var costo_cop = prod_cop_copeo > 0 ? prod_cop_copeo : prod_cop_prec;
+  var costo_sum = prod_sum_odec > 0 ? prod_sum_odec : prod_sum_cuant;
+  kaiz_local["PROFIT"]["PROG"]["BRUTO"] = (admin_est_ppto + admin_ant_ppto) * 0.8 - costo_cop - costo_sum - proy_ppto;
+  kaiz_local["PROFIT"]["PROG"]["NETO"] = profit_prog_bruto * 0.6;
+
+  kaiz_local["PROFIT"]["REAL"]["BRUTO"] = (admin_ant_pag + admin_est_pag) * 0.8 - prod_cop_pag - prod_sum_pag - proy_pag;
+  kaiz_local["PROFIT"]["REAL"]["NETO"] = profit_real_bruto * 0.6;
+}
 
 function sumaEnFirebase(query, cantidad){
     firebase.database().ref(query).once('value').then(function(snapshot){
@@ -76,7 +146,7 @@ function replaceStringInKey(oldString,newString,updates,snapshot,originalSnap){
 
 
 //Recibe el nombre de la hora y regresa un json SCORE:{clave_proceso: horas_trabajadas}
-function loadScoreKaizen(obra){
+/*function loadScoreKaizen(obra){
   var procesos = {};
   firebase.database().ref(rama_bd_obras_magico + "/" + obra).once('value').then(function(snapshot){
     snapshot.child("procesos").forEach(function(proc_snap){
@@ -92,7 +162,7 @@ function loadScoreKaizen(obra){
     console.log(procesos);
     //firebase.database().ref(rama_bd_obras_magico + "/" + obra + "/procesos").update(procesos);
   });
-}
+}*/
 
 function pistaDeAuditoria(){
   var pda = {
@@ -215,4 +285,44 @@ function formatMoney(n, c, d, t) {
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return "$" + s + parts.join(".");
   //return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+var kaiz = {
+    PROYECTOS: {
+        PPTO: 0,
+        PAG: 0,
+    },
+    PRODUCCION: {
+        SUMINISTROS: {
+            CUANT: 0,
+            OdeC: 0,//-1
+            PAG: 0,
+        },
+        COPEO: {
+            PREC: 0,
+            COPEO: 0,//-1
+            PAG: 0,
+        },
+    },
+    ADMINISTRACION: {
+        ESTIMACIONES: {
+            PPTO: 0,
+            EST: 0,
+            PAG: 0,
+        },
+        ANTICIPOS: {
+            PPTO: 0,
+            PAG: 0,
+        },
+    },
+    PROFIT: {
+        PROG: {
+            BRUTO: 0,
+            NETO: 0,
+        },
+        REAL: {
+            BRUTO: 0,
+            NETO: 0,
+        },
+    }
 };
