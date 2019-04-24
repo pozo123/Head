5e6e4ef326c3ca27c91f95c0f897d13c199d9112
pozo123 <diego.pozo@hotmail.com>
2019-04-24 11:09:10 -0500

    obra simple -> misc y reviso si terminada

--- compras/app_compras_asigna_contrato.js
+++ compras/app_compras_asigna_contrato.js
@@ -35,37 +35,39 @@ $("#" + id_obra_ddl_asigna_contrato).change(function(){
 
     firebase.database().ref(rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_asigna_contrato + " option:selected").val()).once('value').then(function(snapshot){
 	    var obra = snapshot.val();
-	    if(obra.num_procesos == 0){
-	    	$('#' + id_group_proc_asigna_contrato).addClass('hidden');
-	    	caso = "obra";
-	    } else {
-			caso = "proc";
-	    	$('#' + id_group_proc_asigna_contrato).removeClass('hidden');
+	    if(!obra.terminada){
+		    if(obra.num_procesos == 0){
+		    	$('#' + id_group_proc_asigna_contrato).addClass('hidden');
+		    	caso = "obra";
+		    } else {
+				caso = "proc";
+		    	$('#' + id_group_proc_asigna_contrato).removeClass('hidden');
 
-		    var select = document.getElementById(id_proc_ddl_asigna_contrato);
-		    var option = document.createElement('option');
-		    option.style = "display:none";
-		    option.text = option.value = "";
-		    select.appendChild(option);
+			    var select = document.getElementById(id_proc_ddl_asigna_contrato);
+			    var option = document.createElement('option');
+			    option.style = "display:none";
+			    option.text = option.value = "";
+			    select.appendChild(option);
 
-		    snapshot.child('procesos').forEach(function(childSnap){
-				var proc = childSnap.val();
-		    	if(proc.num_subprocesos == 0 || obra.nombre == "MARINA NACIONAL 385" || obra.nombre == "IQONO MEXICO"){
-			    	var option2 = document.createElement('OPTION');
-			        option2.text = proc.clave + " (" + proc.nombre + ")";
-			        option2.value = proc.clave;
-			        select.appendChild(option2);
-		    	} else {
-		    		childSnap.child('subprocesos').forEach(function(subpSnap){
-		    			var subp = subpSnap.val();
-		    			var option2 = document.createElement('OPTION');
-				        option2.text = subp.clave + " (" + subp.nombre + ")";
-				        option2.value = subp.clave;
+			    snapshot.child('procesos').forEach(function(childSnap){
+					var proc = childSnap.val();
+			    	if(proc.num_subprocesos == 0 || obra.nombre == "IQONO MEXICO"){
+				    	var option2 = document.createElement('OPTION');
+				        option2.text = proc.clave + " (" + proc.nombre + ")";
+				        option2.value = proc.clave;
 				        select.appendChild(option2);
-		    		});
-		    	}
-		    });
-	    }
+			    	} else {
+			    		childSnap.child('subprocesos').forEach(function(subpSnap){
+			    			var subp = subpSnap.val();
+			    			var option2 = document.createElement('OPTION');
+					        option2.text = subp.clave + " (" + subp.nombre + ")";
+					        option2.value = subp.clave;
+					        select.appendChild(option2);
+			    		});
+			    	}
+			    });
+		    }
+		}
     });
 });
 
@@ -89,7 +91,7 @@ $('#' + id_guardar_button_asigna_contrato).click(function(){
 		console.log(caso);
 		var contrato = $('#' + id_contrato_asigna_contrato).val();
 		if(caso == "obra"){
-			firebase.database().ref(rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_asigna_contrato + " option:selected").val() + "/contrato").set(contrato);
+			firebase.database().ref(rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_asigna_contrato + " option:selected").val() + "/procesos/MISC/contrato").set(contrato);
 		} else if(caso == "proc"){
 			firebase.database().ref(rama_bd_obras_magico + "/" + $('#' + id_obra_ddl_asigna_contrato + " option:selected").val() + "/procesos/" + $('#' + id_proc_ddl_asigna_contrato + " option:selected").val() + "/contrato").set(contrato);
 		} else if(caso == "subp"){
@@ -97,4 +99,4 @@ $('#' + id_guardar_button_asigna_contrato).click(function(){
 		}
 		alert("Numero de contrato asignado con éxito");
 	}
-})
+});
