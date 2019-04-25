49743f570e0d8d6fc5d1e141036d41b69782c95c
pozo123 <diego.pozo@hotmail.com>
2019-04-25 09:39:35 -0500

    reestructura_bd, solo se asigna por obra

--- produccion/app_gestionar_supervisores.js
+++ produccion/app_gestionar_supervisores.js
@@ -1,27 +1,30 @@
 var id_obras_ddl_supervisores = "obraDdlSupervisores";
-var id_supervisores_ddl_supervisores = "supervisoresDdlSupervisores";
+//var id_supervisores_ddl_supervisores = "supervisoresDdlSupervisores";
 var id_div_obra_supervisores = "divObraSupervisores";
 var id_div_supervisor_supervisores = "divSupervisorSupervisores";
 
-var rama_bd_obras_prod = "produccion/obras";
-var rama_bd_obra_magico = "obras";
-var rama_bd_supervisores = "produccion/colaboradores";
-
+var rama_bd_obras_magico = "obras";
+var rama_bd_personal = "personal";
+//rama_bd_supervisores
+//rama_bd_obras_prod
 $('#tabGestionarSup').click(function(){
 
+    /*
     var select = document.getElementById(id_supervisores_ddl_supervisores) ;
     var option = document.createElement('option');
     option.style = "display:none";
     option.text = option.value = "";
     select.appendChild(option);
 
-    firebase.database().ref(rama_bd_supervisores).orderByChild('nombre').on('child_added',function(snapshot){
+    firebase.database().ref(rama_bd_personal).orderByChild('nombre').on('child_added',function(snapshot){
         var sup = snapshot.val();
-        var option2 = document.createElement('OPTION');
-        option2.text = sup.nombre;
-        option2.value = sup.uid;
-        select.appendChild(option2);
-    });
+        if(snapshot.child("areas/produccion").val() && sup.activo){
+            var option2 = document.createElement('OPTION');
+            option2.text = sup.nombre;
+            option2.value = sup.uid;
+            select.appendChild(option2);
+        }
+    });*/
 
     var select2 = document.getElementById(id_obras_ddl_supervisores) ;
     var option3 = document.createElement('option');
@@ -29,22 +32,25 @@ $('#tabGestionarSup').click(function(){
     option3.text = option.value = "";
     select2.appendChild(option3);
 
-    firebase.database().ref(rama_bd_obras_prod).orderByChild('nombre').on('child_added',function(snapshot){
+    firebase.database().ref(rama_bd_obras_magico).orderByChild('nombre').on('child_added',function(snapshot){
         var obra = snapshot.val();
-        var option4 = document.createElement('OPTION');
-        option4.text = obra.nombre;
-        option4.value = obra.nombre;
-        select2.appendChild(option4);
+        if(!obra.terminada){
+            var option4 = document.createElement('OPTION');
+            option4.text = obra.nombre;
+            option4.value = obra.nombre;
+            select2.appendChild(option4);
+        }
     });   
 });
 
 $('#' + id_obras_ddl_supervisores).change(function(){
 	var div = document.getElementById(id_div_obra_supervisores);
+    //document.getElementById(id_supervisores_ddl_supervisores).selectedIndex = 0;
 	$('#' + id_div_obra_supervisores).empty();
 	var nombre_obra = $('#' + id_obras_ddl_supervisores + " option:selected").val();
-	firebase.database().ref(rama_bd_obras_prod + "/" + nombre_obra).once('value').then(function(snapshot){
+	firebase.database().ref(rama_bd_obras_magico + "/" + nombre_obra).once('value').then(function(snapshot){
 		var obra = snapshot.val();
-		snapshot.child("supervisores").forEach(function(childSnap){
+		snapshot.child("supervisor").forEach(function(childSnap){
 			var row = document.createElement('div');
 			var label = document.createElement('label');
 			label.innerHTML = childSnap.val().nombre;
@@ -53,9 +59,7 @@ $('#' + id_obras_ddl_supervisores).change(function(){
 			button.innerHTML = "Eliminar";
 			button.click(function(){
 				var fal = false;
-				firebase.database().ref(rama_bd_supervisores + "/" + this.id + "/obras/" + obra.nombre + "/activa").set(fal);
-                firebase.database().ref(rama_bd_obras_prod + "/" + obra.nombre + "/supervisor/" + this.id + "/activo").set(fal);
-                firebase.database().ref(rama_bd_obra_magico + "/" + obra.nombre + "/supervisor/" + this.id + "/activo").set(fal);
+                firebase.database().ref(rama_bd_obras_magico + "/" + obra.nombre + "/supervisor/" + this.id + "/activo").set(fal);
 			});
 			row.appendChild(label);
 			row.appendChild(button);
@@ -70,40 +74,38 @@ $('#' + id_obras_ddl_supervisores).change(function(){
     option.text = option.value = "";
     select.appendChild(option);
 
-    firebase.database().ref(rama_bd_supervisores).orderByChild('nombre').on('child_added',function(snapshot){
+    firebase.database().ref(rama_bd_personal).orderByChild('nombre').on('child_added',function(snapshot){
         var sup = snapshot.val();
-        var option2 = document.createElement('OPTION');
-        option2.text = sup.nombre;
-        option2.value = sup.uid;
-        select.appendChild(option2);
+        if(snapshot.child("areas/produccion").val() && sup.activo){
+            var option2 = document.createElement('OPTION');
+            option2.text = sup.nombre;
+            option2.value = sup.uid;
+            select.appendChild(option2);
+        }
     });
 
     var buttonAdd = document.createElement('button');
+    buttonAdd.innerHTML = "Agregar";
     buttonAdd.click(function(){
-    	var obr = {
-    		nombre: nombre_obra,
-    		activa: true,
-    	}
     	var sup_uid = $('#supParaObraDdl option:selected').val();
-    	firebase.database().ref(rama_bd_supervisores + "/" + sup_uid + "/obras/" + nombre_obra).set(obr);
-    	
     	var sup = {
     		nombre: $('#supParaObraDdl option:selected').text(),
     		activo: true,
     	}
-        firebase.database().ref(rama_bd_obras_prod + "/" + nombre_obra + "/supervisor/" + sup_uid).set(sup);
-        firebase.database().ref(rama_bd_obra_magico + "/" + nombre_obra + "/supervisor/" + sup_uid).set(sup);
+        firebase.database().ref(rama_bd_obras_magico + "/" + nombre_obra + "/supervisor/" + sup_uid).set(sup);
     });
 
     div.appendChild(select);
     div.appendChild(buttonAdd);
 });
 
+/*
 $('#' + id_supervisores_ddl_supervisores).change(function() {
 	var div = document.getElementById(id_div_supervisor_supervisores);
+    document.getElementById(id_obras_ddl_supervisores).selectedIndex = 0;
 	$('#' + id_div_supervisor_supervisores).empty();
 	var sup_uid = $('#' + id_supervisores_ddl_supervisores + " option:selected").val();
-	firebase.database().ref(rama_bd_supervisores + "/" + sup_uid).once('value').then(function(snapshot){
+	firebase.database().ref(rama_bd_personal + "/" + sup_uid).once('value').then(function(snapshot){
 		var supervisor = snapshot.val();
 		snapshot.child("obras").forEach(function(childSnap){
 			var row = document.createElement('div');
@@ -157,4 +159,4 @@ $('#' + id_supervisores_ddl_supervisores).change(function() {
 
     div.appendChild(select);
     div.appendChild(buttonAdd);
-});
+});*/
