8f05c7be9a896c13676853c0d9968a145c666e6b
pozo123 <diego.pozo@hotmail.com>
2019-04-25 14:50:49 -0500

    reestructura, permisos por creden

--- pag_web/score.js
+++ pag_web/score.js
@@ -1,85 +1,59 @@
-var rama_bd_inges = "proyectos/inges";
+var rama_bd_personal = "personal";
 
 firebase.auth().onAuthStateChanged(user => {
     if(user) {
         userUID = user.uid;
-        var flag = true;
-        firebase.database().ref(rama_bd_inges).orderByChild('uid').equalTo(user.uid).once("child_added", function (snapshot) {
-                if(flag === true){
+        firebase.database().ref(rama_bd_personal + "/" + userUID).once("value").then(function (snapshot) {
 
-                var user_bd = snapshot.val();
+            var user_bd = snapshot.val();
 
-                var usuarioNombre = document.getElementById('usuarioConectado');
-                usuarioNombre.innerHTML = user_bd.nickname;
-                
-                if(user_bd.permisos.alta_colaborador === true)
-                    $('#tabAltaColaborador').removeClass('hidden');
-                else
-                    $('#tabAltaColaborador').addClass('hidden');
-
-                if(user_bd.permisos.alta_obra === true)
-                    $('#tabAltaObra').removeClass('hidden');
-                else
-                    $('#tabAltaObra').addClass('hidden');
-
-                if(user_bd.permisos.alta_cliente === true)
-                    $('#tabAltaCliente').removeClass('hidden');
-                else
-                    $('#tabAltaCliente').addClass('hidden');
-
-                if(user_bd.permisos.alta_exc_reqs === true)
-                    $('#tabAltaReqsExcs').removeClass('hidden');
-                else
-                    $('#tabAltaReqsExcs').addClass('hidden');
-
-                if(user_bd.permisos.alta_generos_tipos === true)
-                    $('#tabAltaTiposGens').removeClass('hidden');
-                else
-                    $('#tabAltaTiposGens').addClass('hidden');
-
-
-                if(user_bd.permisos.reporte === true)
-                    $('#tabReporte').removeClass('hidden');
-                else
-                    $('#tabReporte').addClass('hidden');
-                if(user_bd.permisos.reporte_presupuestos === true)
-                    $('#tabReportePpto').removeClass('hidden');
-                else
-                    $('#tabReportePpto').addClass('hidden');
-
-                    if(user_bd.permisos.perfil === true)
+            var usuarioNombre = document.getElementById('usuarioConectado');
+            usuarioNombre.innerHTML = user_bd.nickname;
+            var creden = user_bd.credenciales;
+            
+            if(creden == 3){
+                $('#tabAltaCliente').addClass('hidden');
+                $('#tabAltaReqsExcs').removeClass('hidden');
+                $('#tabAltaTiposGens').removeClass('hidden');
+                $('#tabReporte').removeClass('hidden');
+                $('#tabReportePpto').removeClass('hidden');
+                $('#tabPerfil').removeClass('hidden');
+                $('#tabActivar').addClass('hidden');
+                $('#tabPagos').addClass('hidden');
+                $('#tabPermisos').addClass('hidden');
+                $('#pageSubmenuCierre').addClass('hidden');
+                $('#pageSubmenuActRegs').addClass('hidden');
+            } else if(creden == 2 || creden == 1 || creden == 0){
+                $('#tabAltaCliente').removeClass('hidden');
+                $('#tabAltaReqsExcs').removeClass('hidden');
+                $('#tabAltaTiposGens').removeClass('hidden');
+                $('#tabReporte').removeClass('hidden');
+                $('#tabReportePpto').removeClass('hidden');
+                $('#tabActivar').removeClass('hidden');
+                $('#tabPagos').removeClass('hidden');
+                $('#pageSubmenuCierre').removeClass('hidden');
+                $('#pageSubmenuActRegs').removeClass('hidden');
+                if(creden == 2){
                     $('#tabPerfil').removeClass('hidden');
-                else
-                    $('#tabPerfil').addClass('hidden');
-                
-                if(user_bd.permisos.activar === true)
-                    $('#tabActivar').removeClass('hidden');
-                else
-                    $('#tabActivar').addClass('hidden');
-
-                if(user_bd.permisos.pagos === true)
-                    $('#tabPagos').removeClass('hidden');
-                else
-                    $('#tabPagos').addClass('hidden');
-
-                if(user_bd.credenciales === 2 || user_bd.credenciales === 1 || user_bd.credenciales === 0)
-                    $('#tabPermisos').removeClass('hidden');
-                else
                     $('#tabPermisos').addClass('hidden');
-                    // agregar botón maestro
-                if(user_bd.credenciales === 2 || user_bd.credenciales === 1 || user_bd.credenciales === 0)
-                    $('#pageSubmenuCierre').removeClass('hidden');
-                else
-                    $('#pageSubmenuCierre').addClass('hidden');
-
-                if(user_bd.credenciales === 0)
-                    $('#pageSubmenuActRegs').removeClass('hidden');
-                else
-                    $('#pageSubmenuActRegs').addClass('hidden');
-                    
-                flag = false;
+                } else {
+                    $('#tabPermisos').removeClass('hidden');
+                    $('#tabPerfil').addClass('hidden');
+                }
+            } else {
+                $('#tabAltaCliente').addClass('hidden');
+                $('#tabAltaReqsExcs').addClass('hidden');
+                $('#tabAltaTiposGens').addClass('hidden');
+                $('#tabReporte').addClass('hidden');
+                $('#tabReportePpto').addClass('hidden');
+                $('#tabPerfil').addClass('hidden');
+                $('#tabActivar').addClass('hidden');
+                $('#tabPagos').addClass('hidden');
+                $('#tabPermisos').addClass('hidden');
+                $('#pageSubmenuCierre').addClass('hidden');
+                $('#pageSubmenuActRegs').addClass('hidden');
             }
-            });
+        });
     } else {
         alert("Inicia sesión para entrar a comunidad");
         window.location.reload("index.html");
