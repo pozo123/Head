f9e7017bc09354db9e1e166ee23c8a21056d928b
artbrare <42916052+artbrare@users.noreply.github.com>
2019-04-26 11:53:57 -0500

    Update app_admon_colaborador.js

--- administracion/app_admon_colaborador.js
+++ administracion/app_admon_colaborador.js
@@ -9,19 +9,33 @@ var id_areas_proy_checkbox_col_admin = "proyAreasCheckboxColAdmin";
 var id_areas_compras_checkbox_col_admin = "comprasAreasCheckboxColAdmin";
 var id_areas_rrhh_checkbox_col_admin = "rrhhAreasCheckboxColAdmin";
 var id_lider_checkbox_col_admin = "liderCheckboxColAdmin";
+var id_group_activo_admin = "groupActivoAdmin"
+
 var id_ie_rb_col_admin = "ieRbColAdmin";//Estos dos del mismo name
+var id_group_ie_admin = "groupIeAdmin"
+
 var id_ihs_rb_col_admin = "ihsRbColAdmin";//Estos dos del mismo name
+var id_group_ihs_admin = "groupIhsAdmin"
+
 var id_activo_checkbox_col_admin = "activoCheckboxAdmin";
+
+var id_form_col_admin = "formColAdmin";
 
 var id_registrar_button_col_admin = "registrarColaboradorAdmin";
 var id_editar_button_col_admin = "editarButtonColAdmin";
+var  id_borrar_button_col_admin = "borrarButtonColAdmin";
 
-var rama_bd_personal = "personal";
+var rama_bd_personal = "test/personal";
 
 $(document).ready(function(){
+
 });
 
 var existe_uid;
+
+$("#" + id_borrar_button_col_admin).click(function(){
+    $('#' + id_email_col_admin).val("");
+});
 
 $("#" + id_email_col_admin).change(function(){
     var existe = false;
@@ -68,8 +82,8 @@ $("#" + id_email_col_admin).change(function(){
                 document.getElementById(id_lider_checkbox_col_admin).disabled = true;
             }
             if(areas.proyectos){
-                $('#' + id_ie_rb_col_admin).removeClass('hidden');
-                $('#' + id_ihs_rb_col_admin).removeClass('hidden');
+                $('#' + id_group_ie_admin).removeClass('hidden');
+                $('#' + id_group_ihs_admin).removeClass('hidden');
                 if(esp == "ie"){
                     document.getElementById(id_ie_rb_col_admin).checked = true;
                 }
@@ -79,13 +93,16 @@ $("#" + id_email_col_admin).change(function(){
             }
 
             $('#' + id_editar_button_col_admin).removeClass('hidden');
-            $('#' + id_activo_checkbox_col_admin).removeClass('hidden');
+            $('#' + id_group_activo_admin).removeClass('hidden');
             $('#' + id_registrar_button_col_admin).addClass('hidden');
             document.getElementById(id_password_col_admin).disabled = true;
 
         } else {
+            document.getElementById(id_form_col_admin).reset();
             $('#' + id_editar_button_col_admin).addClass('hidden');
-            $('#' + id_activo_checkbox_col_admin).addClass('hidden');
+            $('#' + id_group_activo_admin).addClass('hidden');
+            $('#' + id_group_ie_admin).addClass('hidden');
+            $('#' + id_group_ihs_admin).addClass('hidden');
             $('#' + id_registrar_button_col_admin).removeClass('hidden');
             document.getElementById(id_password_col_admin).disabled = false;
             document.getElementById(id_lider_checkbox_col_admin).disabled = false;
@@ -95,11 +112,11 @@ $("#" + id_email_col_admin).change(function(){
 
 $("#" + id_areas_proy_checkbox_col_admin).change(function(){
     if(this.checked){
-        $('#' + id_ie_rb_col_admin).removeClass('hidden');
-        $('#' + id_ihs_rb_col_admin).removeClass('hidden');
+        $('#' + id_group_ie_admin).removeClass('hidden');
+        $('#' + id_group_ihs_admin).removeClass('hidden');
     } else {
-        $('#' + id_ie_rb_col_admin).addClass('hidden');
-        $('#' + id_ihs_rb_col_admin).addClass('hidden');
+        $('#' + id_group_ie_admin).addClass('hidden');
+        $('#' + id_group_ihs_admin).addClass('hidden');
     }
 });
 
@@ -110,6 +127,8 @@ $('#' + id_registrar_button_col_admin).click(function () {
         secondaryApp.auth().createUserWithEmailAndPassword($('#' + id_email_col_admin).val(), $('#' + id_password_col_admin).val())
             .then(function (result) {
                 guardaDatosPersonalAdmin(result.user);
+                console.log(result);
+                console.log(result.user);
                 secondaryApp.auth().signOut();
             }
         );
@@ -162,7 +181,7 @@ $('#' + id_editar_button_col_admin).click(function(){
 
         var persona = {
             nombre: $('#' + id_nombre_col_admin).val(),
-            email: user.email,
+            email: $('#' + id_email_col_admin).val(),
             nickname: $('#' + id_nickname_col_admin).val(),
             areas: areas,
             activo: $('#' + id_activo_checkbox_col_admin).prop('checked'),
@@ -178,7 +197,7 @@ $('#' + id_editar_button_col_admin).click(function(){
         }
 
         if(document.getElementById(id_lider_checkbox_col_admin).disabled == false){
-            persona["credenciales"] = $('#' + id_lider_checkbox_col_admin).prop('checked') ? 2:3,
+            persona["credenciales"] = $('#' + id_lider_checkbox_col_admin).prop('checked') ? 2:3;
         }
 
         firebase.database().ref(rama_bd_personal + "/" + existe_uid).update(persona);
